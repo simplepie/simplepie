6df7833fd73bdc4ebb8cb1c01c38d0e776093e75
Geoffrey Sneddon <geoffrey@simplepie.org>
2006-08-21 13:49:43 +0000

    We should close the connections…

--- test/get_file.php
+++ test/get_file.php
@@ -5,16 +5,16 @@ require('../simplepie.inc'); // Require instead of Include
 
 $file = new SimplePie_File('http://simplepie.org/blog/feed/');
 echo $file->body() . "\r\n";
-unset($file);
+$file->close();
 echo "<hr />\r\n";
 
 $file = new SimplePie_File('http://simplepie.org/');
 echo $file->body() . "\r\n";
-unset($file);
+$file->close();
 echo "<hr />\r\n";
 
 $file = new SimplePie_File('simplepie.org');
 echo $file->body() . "\r\n";
-unset($file);
+$file->close();
 ?>
 <!----------- END SIMPLEPIE TEST ----------->
