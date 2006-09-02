d0a1211f0d7b152151daf3cc61ba09d443278257
Ryan Parman <ryan@simplepie.org>
2006-09-02 19:36:41 +0000

    Updated the demo page to use the new array syntax for the embedding of enclosures.

--- demo/index.php
+++ demo/index.php
@@ -199,7 +199,12 @@ $feed->handle_content_type();
 
 							// Use the embed() method to embed the enclosure into the page inline.
 							echo '<div align="center">';
-							echo '<p>' . $enclosure->embed("audio:./for_the_demo/place_audio.png, video:./for_the_demo/place_video.png, alt:<img src='./for_the_demo/mini_podcast.png' class='download' border='0' title='Download the Podcast (" . $enclosure->get_extension() . "; " . $enclosure->get_size() . " MB)' />, altclass:download") . '</p>';
+							echo '<p>' . $enclosure->embed(array(
+								'audio' => './for_the_demo/place_audio.png',
+								'video' => './for_the_demo/place_video.png',
+								'alt' => '<img src="./for_the_demo/mini_podcast.png" class="download" border="0" title="Download the Podcast (' . $enclosure->get_extension() . '; ' . $enclosure->get_size() . ' MB)" />',
+								'altclass' => 'download'
+							)) . '</p>';
 							echo '<p class="footnote" align="center">(' . $enclosure->get_type() . '; ' . $enclosure->get_size() . ' MB)</p>';
 							echo '</div>';
 						}
