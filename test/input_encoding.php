1df6bf43e350c043568738081bc8c92250ea13ff
Ryan Parman <ryan@simplepie.org>
2006-09-26 01:26:01 +0000

    Test commit.

--- test/input_encoding.php
+++ test/input_encoding.php
@@ -3,17 +3,16 @@
 require('../simplepie.inc');
 
 $feed = new SimplePie();
-$feed->feed_url('http://nurapt.kaist.ac.kr/~jamaica/htmls/blog/rss.php');
+$feed->feed_url('http://gwerneck.libsyn.com/rss');
 $feed->enable_caching(false);
-$feed->input_encoding('EUC-KR'); /* The feed is sending UTF-8, but this is wrong.  We'll override it because we know better. */
 $feed->init();
 
-header('Content-type:text/html; charset=utf-8');
+$feed->handle_content_type();
 
 echo '<h1>' . $feed->get_feed_title() . '</h1>' . "\r\n";
 
 foreach ($feed->get_items() as $item) {
-	echo '<h3><a href="' . $item->get_permalink() . '">' . $item->get_title() . '</a></h3>' . "\r\n";
+	echo '<h3><a href="' . $item->get_permalink() . '">' . utf8_decode($item->get_title()) . '</a></h3>' . "\r\n";
 	echo $item->get_description() . "\r\n";
 }
 
