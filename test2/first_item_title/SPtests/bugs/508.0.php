3ef6a0a92f648ffee0a7122e793ee46c48444e33
Geoffrey Sneddon <geoffrey@simplepie.org>
2006-11-07 17:56:33 +0000

    Redo SimplePie_Misc::strip_comments() fixing the speed issues, commit the phpDoc comments I only saved after commiting #355.

---
+++ test2/first_item_title/SPtests/bugs/508.0.php
@@ -0,0 +1,13 @@
+<?php
+
+$data = '
+<feed xmlns="http://www.w3.org/2005/Atom">
+	<entry>
+		<title>Item <!--' . str_pad('', 100000, 'a') . '--> Title</title>
+	</entry>
+</feed>
+';
+
+$expected = 'Item  Title';
+
+?>
