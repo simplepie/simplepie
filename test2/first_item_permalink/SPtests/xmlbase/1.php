f4bc8b84ff9764355530a8ebd9cc06e52f9846b1
Geoffrey Sneddon <geoffrey@simplepie.org>
2006-11-08 22:14:04 +0000

    Rename entity tests to group them together, add one XHTML test and two base64 tests
    Change SimplePie to add more constants, and to generate the build date dynamically (hopefully)

---
+++ test2/first_item_permalink/SPtests/xmlbase/1.php
@@ -0,0 +1,13 @@
+<?php
+
+$data = <<<EOD
+<feed xmlns="http://www.w3.org/2005/Atom" xml:base="http://example.com/">
+	<entry>
+		<link rel="alternate" href="/alternate" />
+	</entry>
+</feed>
+EOD;
+
+$expected = 'http://example.com/alternate';
+
+?>
---
+++ test2/first_item_title/SPtests/entities/2.4.php
@@ -0,0 +1,13 @@
+<?php
+
+$data = '
+<feed xmlns="http://www.w3.org/2005/Atom">
+	<entry>
+		<title type="application/octet-stream"><![CDATA[' . base64_encode('This &amp;amp; this') . ']]></title>
+	</entry>
+</feed>
+';
+
+$expected = 'This &amp;amp;amp; this';
+
+?>
