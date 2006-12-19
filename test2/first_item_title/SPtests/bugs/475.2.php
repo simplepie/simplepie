deabf286ed4a80ba4ea98924503f3306156f97b9
Geoffrey Sneddon <geoffrey@simplepie.org>
2006-12-19 20:09:50 +0000

    Extension element tests

---
+++ test2/first_item_title/SPtests/bugs/475.2.php
@@ -0,0 +1,16 @@
+<?php
+
+$data = <<<EOD
+<feed xmlns="http://www.w3.org/2005/Atom">
+	<entry>
+		<title>Item Title</title>
+		<x:foo xmlns:x="urn:foo">
+			<title>Extension Title</title>
+		</x>
+	</entry>
+</feed>
+EOD;
+
+$expected = 'Item Title';
+
+?>
