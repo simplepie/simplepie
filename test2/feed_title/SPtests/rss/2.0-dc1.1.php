5ca0837ca07ed2f4fd0297741b82827412105c5f
Geoffrey Sneddon <geoffrey@simplepie.org>
2007-01-05 19:41:19 +0000

    Feed Title RSS/Dublin Core tests

---
+++ test2/feed_title/SPtests/rss/2.0-dc1.1.php
@@ -0,0 +1,13 @@
+<?php
+
+$data = <<<EOD
+<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/">
+	<channel>
+		<dc:title>Feed Title</dc:title>
+	</channel>
+</rss>
+EOD;
+
+$expected = 'Feed Title';
+
+?>
