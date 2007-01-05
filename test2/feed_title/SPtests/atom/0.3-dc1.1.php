412f35b968fb699e0f7b44b3e5f6f3367898c2f9
Geoffrey Sneddon <geoffrey@simplepie.org>
2007-01-05 19:34:58 +0000

    Feed Title Atom/Dublin Core tests

---
+++ test2/feed_title/SPtests/atom/0.3-dc1.1.php
@@ -0,0 +1,11 @@
+<?php
+
+$data = <<<EOD
+<feed version="0.3" xmlns="http://purl.org/atom/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/">
+	<dc:title>Feed Title</dc:title>
+</feed>
+EOD;
+
+$expected = 'Feed Title';
+
+?>
---
+++ test2/feed_title/SPtests/atom/1.0-dc1.1.php
@@ -0,0 +1,11 @@
+<?php
+
+$data = <<<EOD
+<feed xmlns="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/">
+	<dc:title>Feed Title</dc:title>
+</feed>
+EOD;
+
+$expected = 'Feed Title';
+
+?>
