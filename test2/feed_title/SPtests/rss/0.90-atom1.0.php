2dc9cb9886ae483e90d8c850e3eef89c8b63fbdf
Geoffrey Sneddon <geoffrey@simplepie.org>
2007-01-06 16:26:10 +0000

    RSS/Atom 1.0 tests

---
+++ test2/feed_title/SPtests/rss/0.90-atom1.0.php
@@ -0,0 +1,13 @@
+<?php
+
+$data = <<<EOD
+<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns="http://my.netscape.com/rdf/simple/0.9/" xmlns:a="http://www.w3.org/2005/Atom">
+	<channel>
+		<a:title>Feed Title</a:title>
+	</channel>
+</rdf:RDF>
+EOD;
+
+$expected = 'Feed Title';
+
+?>
---
+++ test2/feed_title/SPtests/rss/0.91-userland-atom1.0.php
@@ -0,0 +1,13 @@
+<?php
+
+$data = <<<EOD
+<rss version="0.91" xmlns:a="http://www.w3.org/2005/Atom">
+	<channel>
+		<a:title>Feed Title</a:title>
+	</channel>
+</rss>
+EOD;
+
+$expected = 'Feed Title';
+
+?>
