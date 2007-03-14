f8b0b2274d39ec46ffadfb6c2970c98b0682dda8
Ryan Parman <ryan@simplepie.org>
2007-03-14 05:15:07 +0000

    Added data_array.php to the test folder to get an easy view into what the new data array looks like for a feed.

---
+++ test/data_array.php
@@ -0,0 +1,20 @@
+<?php
+include_once('../simplepie.inc');
+include_once('../idn/idna_convert.class.php');
+
+if (isset($_GET['feed']) && !empty($_GET['feed'])) {
+	$feed = new SimplePie();
+	if (isset($_GET['input']) && !empty($_GET['input'])) {
+		$feed->set_input_encoding($_GET['input']);
+	}
+	if (isset($_GET['fsockopen'])) {
+		$feed->force_fsockopen(true);
+	}
+	$feed->init();
+	$feed->handle_content_type('text/plain');
+	print_r($feed->data);
+}
+else {
+	echo "You must pass a feed URL to the ?feed= parameter in the URL.";
+}
+?>
