1c6012234773880d7db19d7d5b6e37e12d87e92e
Geoffrey Sneddon <geoffrey@simplepie.org>
2007-03-26 17:36:27 +0000

    Time to stop talking and start acting, moving test2 to test

--- test/test2.php
+++
@@ -1,6 +0,0 @@
-<?php
-require('simplepie.inc');
-$feed=new SimplePie();
-echo $feed->get_file('http://www.billsaysthis.com/robots.txt');
-echo $feed->get_file('http://www.billsaysthis.com/movies/?feed=rss2');
-?>
--- test/testes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require('../simplepie.inc');
-$file = new SimplePie_File('http://nymag.com/rss/Movies.xml');
-
-print_r($file);
-
-?>
