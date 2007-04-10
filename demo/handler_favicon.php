33e1663e155f52f209b37d0c08283e3d20dffcbb
Ryan Parman <ryan@simplepie.org>
2007-04-10 01:56:29 +0000

    Added support for caching favicons.  Need to set get_favicon(true) once to cache favicons, then you can just use get_favicon() and it will use the cached favicon.  As I write this, I just thought of a better way do handle it, so I'm going to rewrite part of it so it's a bit more optimal.  Also need to create set_favicon_handler(), which I forgot to do.  In the meantime you can use $feed->favicon_handler='handler_favicon.php';

---
+++ demo/handler_favicon.php
@@ -0,0 +1,19 @@
+<?php
+require_once('../simplepie.inc');
+
+$favicon = new SimplePie();
+$favicon->set_cache_location('../debug/cache');
+$favicon->init();
+
+$cache =& new $favicon->cache_class($favicon->cache_location, call_user_func($favicon->cache_name_function, $_GET['i']), 'favicon');
+
+if ($file = $cache->load())
+{
+	header('Content-type:' . $file['headers']['Content-type']);
+	echo $file['body'];
+	exit;
+}
+
+die('Cached favicon file cannot be found.');
+
+?>
