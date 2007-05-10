1d49850a772b16d86bbd214761ccf0a51844f14d
Geoffrey Sneddon <geoffrey@simplepie.org>
2007-05-10 16:39:05 +0000

    Stage 8

--- test/feed_image_width/SPtests/atom/1.0/icon-default.php
+++ test/feed_image_width/SPtests/atom/1.0/icon-default.php
@@ -1,12 +1,12 @@
 <?php
 
-class SimplePie_Feed_Image_Width_Test_Atom_10 extends SimplePie_Feed_Image_Width_Test
+class SimplePie_Feed_Image_Width_Test_Atom_10_Icon_Default extends SimplePie_Feed_Image_Width_Test
 {
 	function data()
 	{
 		$this->data = 
 '<feed xmlns="http://www.w3.org/2005/Atom">
-	<logo>http://example.com/</logo>
+	<icon>http://example.com/</icon>
 </feed>';
 	}
 	
--- test/feed_image_width/SPtests/atom/1.0/logo-default.php
+++ test/feed_image_width/SPtests/atom/1.0/logo-default.php
@@ -1,11 +1,11 @@
 <?php
 
-class SimplePie_Feed_Image_Width_Test_Atom_03 extends SimplePie_Feed_Image_Width_Test
+class SimplePie_Feed_Image_Width_Test_Atom_10_Logo_Default extends SimplePie_Feed_Image_Width_Test
 {
 	function data()
 	{
 		$this->data = 
-'<feed version="0.3" xmlns="http://purl.org/atom/ns#">
+'<feed xmlns="http://www.w3.org/2005/Atom">
 	<logo>http://example.com/</logo>
 </feed>';
 	}
