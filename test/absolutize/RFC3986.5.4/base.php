48ecbd9c97c567dd52ab1b7a57b25a1102820fe1
Geoffrey Sneddon <geoffrey@simplepie.org>
2007-07-01 19:48:27 +0000

    No need for is_a() here. We really should support PHP4.1 now (needs testing, though).

--- test/absolutize/RFC3986.5.4/base.php
+++ test/absolutize/RFC3986.5.4/base.php
@@ -5,7 +5,7 @@ class SimplePie_Absolutize_Test_RFC3986 extends SimplePie_Absolutize_Test
 	function SimplePie_Absolutize_Test_RFC3986()
 	{
 		// Ugly hack so it only applies to this and none of its children
-		if (is_a($this, 'SimplePie_Absolutize_Test_RFC3986') && !is_subclass_of($this, 'SimplePie_Absolutize_Test_RFC3986'))
+		if (!is_subclass_of($this, 'SimplePie_Absolutize_Test_RFC3986'))
 		{
 			$this->test = false;
 		}
