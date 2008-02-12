f90f20a03e5f7bb68425a389a85efeeaab4e369d
Geoffrey Sneddon <geoffrey@simplepie.org>
2008-02-12 16:14:02 +0000

    Go back to using native line breaks, and just hard-code "\x0A" in trunk/test/absolutize/SPtests/bugs/579.0.php (fixing #31).

--- test/absolutize/SPtests/bugs/579.0.php
+++ test/absolutize/SPtests/bugs/579.0.php
@@ -5,8 +5,7 @@ class SimplePie_Absolutize_Test_Bug_579_Test_0 extends SimplePie_Absolutize_Test
 	function data()
 	{
 		$this->data['base'] = 'http://a/b/';
-		$this->data['relative'] = 'b
-c';
+		$this->data['relative'] = "b\x0Ac";
 	}
 	
 	function expected()
