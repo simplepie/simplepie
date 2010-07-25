229bcef2a49a56f4b09a7ef4df22a971e3e7c708
Drak <drak@zikula.org>
2010-07-25 16:48:47 +0545

    Fix build script.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cat SimplePie.php > SimplePie.compiled.php 
-find SimplePie -type f| xargs cat | sed 's/<?php//g'>>SimplePie.compiled.php
+cat SimplePie.php > SimplePie.compiled.php
+find SimplePie -type f| xargs cat | sed 's/^<?php//g'>>SimplePie.compiled.php
 
