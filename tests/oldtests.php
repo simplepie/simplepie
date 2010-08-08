1817c67580f26993b517a6526c672904ab2196cd
Geoffrey Sneddon <geoffers@gmail.com>
2010-08-08 15:33:27 +0200

    The big move of test to tests, and move to PHPUnit.

---
+++ tests/oldtests.php
@@ -0,0 +1,114 @@
+<?php
+
+date_default_timezone_set('UTC');
+error_reporting(E_ALL | E_STRICT);
+
+require_once 'PHPUnit/Framework.php';
+require_once '../simplepie.class.php';
+require_once 'oldtests/compat_test_harness.php';
+require_once 'oldtests/functions.php';
+
+class OldTest extends PHPUnit_Framework_TestCase
+{
+    public function getTests()
+    {
+        $test_folders = array(
+            'absolutize',
+            'date',
+            'feed_category_label',
+            'feed_copyright',
+            'feed_description',
+            'feed_image_height',
+            'feed_image_link',
+            'feed_image_title',
+            'feed_image_url',
+            'feed_image_width',
+            'feed_language',
+            'feed_link',
+            'feed_title',
+            'first_item_author_name',
+            'first_item_category_label',
+            'first_item_content',
+            'first_item_contributor_name',
+            'first_item_date',
+            'first_item_description',
+            'first_item_id',
+            'first_item_latitude',
+            'first_item_longitude',
+            'first_item_permalink',
+            'first_item_title',
+            'functions.php',
+            'itunes_rss',
+            'who_knows_a_title_from_a_hole_in_the_ground'
+        );
+        
+        $master = new Unit_Test2_Group('SimplePie Test Suite');
+        
+        foreach ($test_folders as $test)
+        {
+            $test_group = new SimplePie_Unit_Test2_Group(ucwords(str_replace('_', ' ', $test)));
+            $test_group->load_folder(dirname(__FILE__) . '/oldtests/' . $test);
+            $master->add($test_group);
+        }
+        
+        $test_group = new SimplePie_Unit_Test2_Group('Who knows a <title> from a hole in the ground?');
+        $test_group->load_folder(dirname(__FILE__) . '/oldtests/who_knows_a_title_from_a_hole_in_the_ground');
+        $master->add($test_group);
+        
+        $tests = array();
+        $groups = array($master);
+        while ($group = array_shift($groups))
+        {
+            foreach ($group->tests as $group_tests)
+            {
+                foreach ($group_tests as $test)
+                {
+                    if ($test instanceof Unit_Test2)
+                    {
+                        $tests[] = array($test);
+                    }
+                    elseif ($test instanceof Unit_Test2_Group)
+                    {
+                        $groups[] = $test;
+                    }
+                }
+            }
+        }
+        return $tests;
+    }
+ 
+    /**
+     * @dataProvider getTests
+     */
+    public function testOld($test)
+    {
+        $test->run();
+        if ($test->result instanceof \ComplexPie\Content)
+        {
+            if ($test->result instanceof \ComplexPie\Content_Node)
+            {
+                $node = $test->result->get_node();
+                if (is_array($node))
+                {
+                    $baseURI = $node[0]->baseURI;
+                }
+                else
+                {
+                    $baseURI = $node->baseURI;
+                }
+            }
+            else
+            {
+                $baseURI = null;
+            }
+            $sanitizer = new \ComplexPie\Sanitize();
+            // The 4 below should be \ComplexPie\CONSTRUCT_XHTML, but that errors. WTF PHP?
+            $test->result = $sanitizer->dosanitize($test->result->to_xml(), 4, $baseURI);
+        }
+        elseif ($test->result instanceof \DateTime)
+        {
+            $test->result = $test->result->getTimestamp();
+        }
+        $this->assertSame($test->expected, $test->result);
+    }
+}
