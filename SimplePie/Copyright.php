018e0d4a86728a508ced77c6fce8205245b89fe6
Ryan McCue <me@ryanmccue.info>
2012-01-16 00:31:30 +1000

    Add PHPDoc for Copyright

--- SimplePie/Copyright.php
+++ SimplePie/Copyright.php
@@ -54,22 +54,50 @@
  */
 class SimplePie_Copyright
 {
+	/**
+	 * Copyright URL
+	 *
+	 * @var string
+	 * @see get_url()
+	 */
 	var $url;
+
+	/**
+	 * Attribution
+	 *
+	 * @var string
+	 * @see get_attribution()
+	 */
 	var $label;
 
-	// Constructor, used to input the data
+	/**
+	 * Constructor, used to input the data
+	 *
+	 * For documentation on all the parameters, see the corresponding
+	 * properties and their accessors
+	 */
 	public function __construct($url = null, $label = null)
 	{
 		$this->url = $url;
 		$this->label = $label;
 	}
 
+	/**
+	 * String-ified version
+	 *
+	 * @return string
+	 */
 	public function __toString()
 	{
 		// There is no $this->data here
 		return md5(serialize($this));
 	}
 
+	/**
+	 * Get the copyright URL
+	 *
+	 * @return string|null URL to copyright information
+	 */
 	public function get_url()
 	{
 		if ($this->url !== null)
@@ -82,6 +110,11 @@ class SimplePie_Copyright
 		}
 	}
 
+	/**
+	 * Get the attribution text
+	 *
+	 * @return string|null
+	 */
 	public function get_attribution()
 	{
 		if ($this->label !== null)
