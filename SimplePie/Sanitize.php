013ee9c5cd164c4f229e69c7d5514ebe3fdd5709
Ryan McCue <me@ryanmccue.info>
2012-07-02 16:40:20 +1000

    Use the correct DOCTYPEs when sanitizing

--- SimplePie/Sanitize.php
+++ SimplePie/Sanitize.php
@@ -365,13 +365,13 @@ class SimplePie_Sanitize
 			// Atom XHTML constructs are wrapped with a div by default
 			// Note: No protection if $html contains a stray </div>!
 			$html = '<div>' . $html . '</div>';
-			$ret .= '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
-			$content_type = 'application/xhtml+xml';
+			$ret .= '<!DOCTYPE html>';
+			$content_type = 'text/html';
 		}
 		else
 		{
-			$ret .= '<!DOCTYPE html>';
-			$content_type = 'text/html';
+			$ret .= '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
+			$content_type = 'application/xhtml+xml';
 		}
 
 		$ret .= '<html><head>';
