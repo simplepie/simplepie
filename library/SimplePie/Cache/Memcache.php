378cb1ed16bd8561101bef30ae917b39c1ab5060
Ryan McCue <me@ryanmccue.info>
2012-07-07 17:51:11 +1000

    Merge options in Memcache handler properly
    `array_merge_recursive()` merges two strings into an array, which means
    breakages if overriding the host. This should fix #210, thanks to @n1c

--- library/SimplePie/Cache/Memcache.php
+++ library/SimplePie/Cache/Memcache.php
@@ -95,7 +95,10 @@ class SimplePie_Cache_Memcache implements SimplePie_Cache_Base
 				'prefix' => 'simplepie_',
 			),
 		);
-		$this->options = array_merge_recursive($this->options, SimplePie_Cache::parse_URL($location));
+		$parsed = SimplePie_Cache::parse_URL($location);
+		$this->options['host'] = empty($parsed['host']) ? $this->options['host'] : $parsed['host'];
+		$this->options['port'] = empty($parsed['port']) ? $this->options['port'] : $parsed['port'];
+		$this->options['extras'] = array_merge($this->options['extras'], $parsed['extras']);
 		$this->name = $this->options['extras']['prefix'] . md5("$name:$type");
 
 		$this->cache = new Memcache();
