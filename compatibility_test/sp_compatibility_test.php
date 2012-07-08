a7307d89cdd795122bde778974d6b8ef318e5a63
Ryan McCue <me@ryanmccue.info>
2012-07-08 15:08:53 +1000

    Bump to 1.4-dev

--- compatibility_test/sp_compatibility_test.php
+++ compatibility_test/sp_compatibility_test.php
@@ -42,7 +42,7 @@ header('Content-type: text/html; charset=UTF-8');
 
 <html lang="en">
 <head>
-<title>SimplePie: Server Compatibility Test 1.3</title>
+<title>SimplePie: Server Compatibility Test 1.4-dev</title>
 
 <style type="text/css">
 body {
