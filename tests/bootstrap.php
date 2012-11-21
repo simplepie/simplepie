bb48637466d6a5a998cde6e78e874bc4bb2d3584
Ryan McCue <me@ryanmccue.info>
2012-11-21 22:56:26 +1000

    Fix tests on PHP 5.2
    Not quite sure why this was occurring.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,20 @@
 <?php
 
 require_once dirname(dirname(__FILE__)) . '/autoloader.php';
+
+/**
+ * Acts as a fake feed request
+ */
+class MockSimplePie_File extends SimplePie_File
+{
+	public function __construct($url)
+	{
+		$this->url = $url;
+		$this->headers = array(
+			'content-type' => 'application/atom+xml'
+		);
+		$this->method = SIMPLEPIE_FILE_SOURCE_REMOTE;
+		$this->body = '<?xml charset="utf-8"?><feed />';
+		$this->status_code = 200;
+	}
+}
