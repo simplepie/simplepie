3a6245f4c70e303afffb030017a8d8f0fd560f05
devlinjunker <devlin.junker@gmail.com>
2013-02-28 23:18:05 -0800

    fixed MySQL URI Parse Array Merge

--- library/SimplePie/Cache/MySQL.php
+++ library/SimplePie/Cache/MySQL.php
@@ -96,7 +96,7 @@ class SimplePie_Cache_MySQL extends SimplePie_Cache_DB
 				'prefix' => '',
 			),
 		);
-		$this->options = array_merge_recursive($this->options, SimplePie_Cache::parse_URL($location));
+		$this->options = array_merge($this->options, SimplePie_Cache::parse_URL($location));
 
 		// Path is prefixed with a "/"
 		$this->options['dbname'] = substr($this->options['path'], 1);
