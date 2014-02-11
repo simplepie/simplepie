deadb9005a7f6165c0a02615fe5847dd02be04de
Malcolm Blaney <mblaney@gmail.com>
2014-02-11 11:10:54 +1000

    Check that absolutize doesn't return false.

--- library/SimplePie/Misc.php
+++ library/SimplePie/Misc.php
@@ -79,8 +79,9 @@ class SimplePie_Misc
 
 	public static function absolutize_url($relative, $base)
 	{
-		$iri = SimplePie_IRI::absolutize(new SimplePie_IRI($base), $relative);
-		return $iri->get_uri();
+		if ($iri = SimplePie_IRI::absolutize(new SimplePie_IRI($base), $relative)) {
+			return $iri->get_uri();
+		}
 	}
 
 	/**
