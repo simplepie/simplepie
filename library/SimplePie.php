7f6d7ba8503dd2574b733df907033e78c8d67aad
Alexandre Alapetite <alexandre@alapetite.fr>
2020-03-30 09:55:43 +0200

    Revert second case

--- library/SimplePie.php
+++ library/SimplePie.php
@@ -1711,8 +1711,7 @@ class SimplePie
 					{
 						trigger_error("$this->cache_location is not writable. Make sure you've set the correct relative or absolute path, and that the location is server-writable.", E_USER_WARNING);
 					}
-					$url = $this->feed_url . '#force_feed';
-					$cache = $this->registry->call('Cache', 'get_handler', array($this->cache_location, call_user_func($this->cache_name_function, $url), 'spc'));
+					$cache = $this->registry->call('Cache', 'get_handler', array($this->cache_location, call_user_func($this->cache_name_function, $file->url), 'spc'));
 				}
 			}
 			$this->feed_url = $file->url;
