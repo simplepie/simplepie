22cd34417519a765070ae1b2a36a64d446bd607a
Alexandre Alapetite <alexandre@alapetite.fr>
2021-04-13 08:21:51 +0200

    Fix typo in comment From https://github.com/simplepie/simplepie/pull/676 A parenthesis was misplaced

--- library/SimplePie/Sanitize.php
+++ library/SimplePie/Sanitize.php
@@ -76,7 +76,7 @@ class SimplePie_Sanitize
 	 * List of domains for which to force HTTPS.
 	 * @see SimplePie_Sanitize::set_https_domains()
 	 * Array is a tree split at DNS levels. Example:
-	 * array('biz' => true, 'com' => array('example' => true), 'net' => array('example') => array('www' => true))
+	 * array('biz' => true, 'com' => array('example' => true), 'net' => array('example' => array('www' => true)))
 	 */
 	var $https_domains = array();
 
