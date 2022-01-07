6a69d395373e7ab5f64d75a72df72d42ee7ae0de
rdalverny <rdalverny@gmail.com>
2022-01-07 18:16:58 +0100

    Add audio, video @src elements/attribute for URL resolution
    Independently from enclosures, it happens that audio/video contents are also embedded through `<audio src="" />` and `<video src="" />` tags.
    Those deserve their source URLs to be properly resolved too. 😛 
    
    However I'm not 100% aware of the history of SimplePie, so there may be a reason those were left out (other than they're just rather recent elements). Thanks!

--- library/SimplePie/Sanitize.php
+++ library/SimplePie/Sanitize.php
@@ -221,9 +221,9 @@ class SimplePie_Sanitize
 	 * Set element/attribute key/value pairs of HTML attributes
 	 * containing URLs that need to be resolved relative to the feed
 	 *
-	 * Defaults to |a|@href, |area|@href, |blockquote|@cite, |del|@cite,
-	 * |form|@action, |img|@longdesc, |img|@src, |input|@src, |ins|@cite,
-	 * |q|@cite
+	 * Defaults to |a|@href, |area|@href, |audio|@src, |blockquote|@cite,
+	 * |del|@cite, |form|@action, |img|@longdesc, |img|@src, |input|@src,
+	 * |ins|@cite, |q|@cite, |video|@src
 	 *
 	 * @since 1.0
 	 * @param array|null $element_attribute Element/attribute key/value pairs, null for default
@@ -235,6 +235,7 @@ class SimplePie_Sanitize
 			$element_attribute = array(
 				'a' => 'href',
 				'area' => 'href',
+				'audio' => 'src',
 				'blockquote' => 'cite',
 				'del' => 'cite',
 				'form' => 'action',
@@ -244,7 +245,8 @@ class SimplePie_Sanitize
 				),
 				'input' => 'src',
 				'ins' => 'cite',
-				'q' => 'cite'
+				'q' => 'cite',
+				'video' => 'src'
 			);
 		}
 		$this->replace_url_attributes = (array) $element_attribute;
