6f2489060ae963bc469fb8225f2cac5dcea7f3fc
Art4 <art4@wlabs.de>
2022-02-20 21:29:25 +0100

    Move most global constants into SimplePie class

--- library/SimplePie.php
+++ library/SimplePie.php
@@ -42,366 +42,6 @@
  * @license http://www.opensource.org/licenses/bsd-license.php BSD License
  */
 
-/**
- * SimplePie Name
- */
-define('SIMPLEPIE_NAME', 'SimplePie');
-
-/**
- * SimplePie Version
- */
-define('SIMPLEPIE_VERSION', '1.5.8');
-
-/**
- * SimplePie Build
- * @todo Hardcode for release (there's no need to have to call SimplePie_Misc::get_build() only every load of simplepie.inc)
- */
-define('SIMPLEPIE_BUILD', gmdate('YmdHis', SimplePie_Misc::get_build()));
-
-/**
- * SimplePie Website URL
- */
-define('SIMPLEPIE_URL', 'http://simplepie.org');
-
-/**
- * SimplePie Useragent
- * @see SimplePie::set_useragent()
- */
-define('SIMPLEPIE_USERAGENT', SIMPLEPIE_NAME . '/' . SIMPLEPIE_VERSION . ' (Feed Parser; ' . SIMPLEPIE_URL . '; Allow like Gecko) Build/' . SIMPLEPIE_BUILD);
-
-/**
- * SimplePie Linkback
- */
-define('SIMPLEPIE_LINKBACK', '<a href="' . SIMPLEPIE_URL . '" title="' . SIMPLEPIE_NAME . ' ' . SIMPLEPIE_VERSION . '">' . SIMPLEPIE_NAME . '</a>');
-
-/**
- * No Autodiscovery
- * @see SimplePie::set_autodiscovery_level()
- */
-define('SIMPLEPIE_LOCATOR_NONE', 0);
-
-/**
- * Feed Link Element Autodiscovery
- * @see SimplePie::set_autodiscovery_level()
- */
-define('SIMPLEPIE_LOCATOR_AUTODISCOVERY', 1);
-
-/**
- * Local Feed Extension Autodiscovery
- * @see SimplePie::set_autodiscovery_level()
- */
-define('SIMPLEPIE_LOCATOR_LOCAL_EXTENSION', 2);
-
-/**
- * Local Feed Body Autodiscovery
- * @see SimplePie::set_autodiscovery_level()
- */
-define('SIMPLEPIE_LOCATOR_LOCAL_BODY', 4);
-
-/**
- * Remote Feed Extension Autodiscovery
- * @see SimplePie::set_autodiscovery_level()
- */
-define('SIMPLEPIE_LOCATOR_REMOTE_EXTENSION', 8);
-
-/**
- * Remote Feed Body Autodiscovery
- * @see SimplePie::set_autodiscovery_level()
- */
-define('SIMPLEPIE_LOCATOR_REMOTE_BODY', 16);
-
-/**
- * All Feed Autodiscovery
- * @see SimplePie::set_autodiscovery_level()
- */
-define('SIMPLEPIE_LOCATOR_ALL', 31);
-
-/**
- * No known feed type
- */
-define('SIMPLEPIE_TYPE_NONE', 0);
-
-/**
- * RSS 0.90
- */
-define('SIMPLEPIE_TYPE_RSS_090', 1);
-
-/**
- * RSS 0.91 (Netscape)
- */
-define('SIMPLEPIE_TYPE_RSS_091_NETSCAPE', 2);
-
-/**
- * RSS 0.91 (Userland)
- */
-define('SIMPLEPIE_TYPE_RSS_091_USERLAND', 4);
-
-/**
- * RSS 0.91 (both Netscape and Userland)
- */
-define('SIMPLEPIE_TYPE_RSS_091', 6);
-
-/**
- * RSS 0.92
- */
-define('SIMPLEPIE_TYPE_RSS_092', 8);
-
-/**
- * RSS 0.93
- */
-define('SIMPLEPIE_TYPE_RSS_093', 16);
-
-/**
- * RSS 0.94
- */
-define('SIMPLEPIE_TYPE_RSS_094', 32);
-
-/**
- * RSS 1.0
- */
-define('SIMPLEPIE_TYPE_RSS_10', 64);
-
-/**
- * RSS 2.0
- */
-define('SIMPLEPIE_TYPE_RSS_20', 128);
-
-/**
- * RDF-based RSS
- */
-define('SIMPLEPIE_TYPE_RSS_RDF', 65);
-
-/**
- * Non-RDF-based RSS (truly intended as syndication format)
- */
-define('SIMPLEPIE_TYPE_RSS_SYNDICATION', 190);
-
-/**
- * All RSS
- */
-define('SIMPLEPIE_TYPE_RSS_ALL', 255);
-
-/**
- * Atom 0.3
- */
-define('SIMPLEPIE_TYPE_ATOM_03', 256);
-
-/**
- * Atom 1.0
- */
-define('SIMPLEPIE_TYPE_ATOM_10', 512);
-
-/**
- * All Atom
- */
-define('SIMPLEPIE_TYPE_ATOM_ALL', 768);
-
-/**
- * All feed types
- */
-define('SIMPLEPIE_TYPE_ALL', 1023);
-
-/**
- * No construct
- */
-define('SIMPLEPIE_CONSTRUCT_NONE', 0);
-
-/**
- * Text construct
- */
-define('SIMPLEPIE_CONSTRUCT_TEXT', 1);
-
-/**
- * HTML construct
- */
-define('SIMPLEPIE_CONSTRUCT_HTML', 2);
-
-/**
- * XHTML construct
- */
-define('SIMPLEPIE_CONSTRUCT_XHTML', 4);
-
-/**
- * base64-encoded construct
- */
-define('SIMPLEPIE_CONSTRUCT_BASE64', 8);
-
-/**
- * IRI construct
- */
-define('SIMPLEPIE_CONSTRUCT_IRI', 16);
-
-/**
- * A construct that might be HTML
- */
-define('SIMPLEPIE_CONSTRUCT_MAYBE_HTML', 32);
-
-/**
- * All constructs
- */
-define('SIMPLEPIE_CONSTRUCT_ALL', 63);
-
-/**
- * Don't change case
- */
-define('SIMPLEPIE_SAME_CASE', 1);
-
-/**
- * Change to lowercase
- */
-define('SIMPLEPIE_LOWERCASE', 2);
-
-/**
- * Change to uppercase
- */
-define('SIMPLEPIE_UPPERCASE', 4);
-
-/**
- * PCRE for HTML attributes
- */
-define('SIMPLEPIE_PCRE_HTML_ATTRIBUTE', '((?:[\x09\x0A\x0B\x0C\x0D\x20]+[^\x09\x0A\x0B\x0C\x0D\x20\x2F\x3E][^\x09\x0A\x0B\x0C\x0D\x20\x2F\x3D\x3E]*(?:[\x09\x0A\x0B\x0C\x0D\x20]*=[\x09\x0A\x0B\x0C\x0D\x20]*(?:"(?:[^"]*)"|\'(?:[^\']*)\'|(?:[^\x09\x0A\x0B\x0C\x0D\x20\x22\x27\x3E][^\x09\x0A\x0B\x0C\x0D\x20\x3E]*)?))?)*)[\x09\x0A\x0B\x0C\x0D\x20]*');
-
-/**
- * PCRE for XML attributes
- */
-define('SIMPLEPIE_PCRE_XML_ATTRIBUTE', '((?:\s+(?:(?:[^\s:]+:)?[^\s:]+)\s*=\s*(?:"(?:[^"]*)"|\'(?:[^\']*)\'))*)\s*');
-
-/**
- * XML Namespace
- */
-define('SIMPLEPIE_NAMESPACE_XML', 'http://www.w3.org/XML/1998/namespace');
-
-/**
- * Atom 1.0 Namespace
- */
-define('SIMPLEPIE_NAMESPACE_ATOM_10', 'http://www.w3.org/2005/Atom');
-
-/**
- * Atom 0.3 Namespace
- */
-define('SIMPLEPIE_NAMESPACE_ATOM_03', 'http://purl.org/atom/ns#');
-
-/**
- * RDF Namespace
- */
-define('SIMPLEPIE_NAMESPACE_RDF', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#');
-
-/**
- * RSS 0.90 Namespace
- */
-define('SIMPLEPIE_NAMESPACE_RSS_090', 'http://my.netscape.com/rdf/simple/0.9/');
-
-/**
- * RSS 1.0 Namespace
- */
-define('SIMPLEPIE_NAMESPACE_RSS_10', 'http://purl.org/rss/1.0/');
-
-/**
- * RSS 1.0 Content Module Namespace
- */
-define('SIMPLEPIE_NAMESPACE_RSS_10_MODULES_CONTENT', 'http://purl.org/rss/1.0/modules/content/');
-
-/**
- * RSS 2.0 Namespace
- * (Stupid, I know, but I'm certain it will confuse people less with support.)
- */
-define('SIMPLEPIE_NAMESPACE_RSS_20', '');
-
-/**
- * DC 1.0 Namespace
- */
-define('SIMPLEPIE_NAMESPACE_DC_10', 'http://purl.org/dc/elements/1.0/');
-
-/**
- * DC 1.1 Namespace
- */
-define('SIMPLEPIE_NAMESPACE_DC_11', 'http://purl.org/dc/elements/1.1/');
-
-/**
- * W3C Basic Geo (WGS84 lat/long) Vocabulary Namespace
- */
-define('SIMPLEPIE_NAMESPACE_W3C_BASIC_GEO', 'http://www.w3.org/2003/01/geo/wgs84_pos#');
-
-/**
- * GeoRSS Namespace
- */
-define('SIMPLEPIE_NAMESPACE_GEORSS', 'http://www.georss.org/georss');
-
-/**
- * Media RSS Namespace
- */
-define('SIMPLEPIE_NAMESPACE_MEDIARSS', 'http://search.yahoo.com/mrss/');
-
-/**
- * Wrong Media RSS Namespace. Caused by a long-standing typo in the spec.
- */
-define('SIMPLEPIE_NAMESPACE_MEDIARSS_WRONG', 'http://search.yahoo.com/mrss');
-
-/**
- * Wrong Media RSS Namespace #2. New namespace introduced in Media RSS 1.5.
- */
-define('SIMPLEPIE_NAMESPACE_MEDIARSS_WRONG2', 'http://video.search.yahoo.com/mrss');
-
-/**
- * Wrong Media RSS Namespace #3. A possible typo of the Media RSS 1.5 namespace.
- */
-define('SIMPLEPIE_NAMESPACE_MEDIARSS_WRONG3', 'http://video.search.yahoo.com/mrss/');
-
-/**
- * Wrong Media RSS Namespace #4. New spec location after the RSS Advisory Board takes it over, but not a valid namespace.
- */
-define('SIMPLEPIE_NAMESPACE_MEDIARSS_WRONG4', 'http://www.rssboard.org/media-rss');
-
-/**
- * Wrong Media RSS Namespace #5. A possible typo of the RSS Advisory Board URL.
- */
-define('SIMPLEPIE_NAMESPACE_MEDIARSS_WRONG5', 'http://www.rssboard.org/media-rss/');
-
-/**
- * iTunes RSS Namespace
- */
-define('SIMPLEPIE_NAMESPACE_ITUNES', 'http://www.itunes.com/dtds/podcast-1.0.dtd');
-
-/**
- * XHTML Namespace
- */
-define('SIMPLEPIE_NAMESPACE_XHTML', 'http://www.w3.org/1999/xhtml');
-
-/**
- * IANA Link Relations Registry
- */
-define('SIMPLEPIE_IANA_LINK_RELATIONS_REGISTRY', 'http://www.iana.org/assignments/relation/');
-
-/**
- * No file source
- */
-define('SIMPLEPIE_FILE_SOURCE_NONE', 0);
-
-/**
- * Remote file source
- */
-define('SIMPLEPIE_FILE_SOURCE_REMOTE', 1);
-
-/**
- * Local file source
- */
-define('SIMPLEPIE_FILE_SOURCE_LOCAL', 2);
-
-/**
- * fsockopen() file source
- */
-define('SIMPLEPIE_FILE_SOURCE_FSOCKOPEN', 4);
-
-/**
- * cURL file source
- */
-define('SIMPLEPIE_FILE_SOURCE_CURL', 8);
-
-/**
- * file_get_contents() file source
- */
-define('SIMPLEPIE_FILE_SOURCE_FILE_GET_CONTENTS', 16);
-
 use SimplePie\SimplePie as NamespacedSimplePie;
 
 class_exists('SimplePie\SimplePie');
@@ -414,3 +54,363 @@ if (\false) {
 	{
 	}
 }
+
+/**
+ * SimplePie Name
+ */
+define('SIMPLEPIE_NAME', NamespacedSimplePie::SIMPLEPIE_NAME);
+
+/**
+ * SimplePie Version
+ */
+define('SIMPLEPIE_VERSION', NamespacedSimplePie::SIMPLEPIE_VERSION);
+
+/**
+ * SimplePie Build
+ * @todo Hardcode for release (there's no need to have to call SimplePie_Misc::get_build() only every load of simplepie.inc)
+ */
+define('SIMPLEPIE_BUILD', gmdate('YmdHis', \SimplePie\Misc::get_build()));
+
+/**
+ * SimplePie Website URL
+ */
+define('SIMPLEPIE_URL', NamespacedSimplePie::SIMPLEPIE_URL);
+
+/**
+ * SimplePie Useragent
+ * @see SimplePie::set_useragent()
+ */
+define('SIMPLEPIE_USERAGENT', NamespacedSimplePie::SIMPLEPIE_USERAGENT);
+
+/**
+ * SimplePie Linkback
+ */
+define('SIMPLEPIE_LINKBACK', NamespacedSimplePie::SIMPLEPIE_LINKBACK);
+
+/**
+ * No Autodiscovery
+ * @see SimplePie::set_autodiscovery_level()
+ */
+define('SIMPLEPIE_LOCATOR_NONE', NamespacedSimplePie::SIMPLEPIE_LOCATOR_NONE);
+
+/**
+ * Feed Link Element Autodiscovery
+ * @see SimplePie::set_autodiscovery_level()
+ */
+define('SIMPLEPIE_LOCATOR_AUTODISCOVERY', NamespacedSimplePie::SIMPLEPIE_LOCATOR_AUTODISCOVERY);
+
+/**
+ * Local Feed Extension Autodiscovery
+ * @see SimplePie::set_autodiscovery_level()
+ */
+define('SIMPLEPIE_LOCATOR_LOCAL_EXTENSION', NamespacedSimplePie::SIMPLEPIE_LOCATOR_LOCAL_EXTENSION);
+
+/**
+ * Local Feed Body Autodiscovery
+ * @see SimplePie::set_autodiscovery_level()
+ */
+define('SIMPLEPIE_LOCATOR_LOCAL_BODY', NamespacedSimplePie::SIMPLEPIE_LOCATOR_LOCAL_BODY);
+
+/**
+ * Remote Feed Extension Autodiscovery
+ * @see SimplePie::set_autodiscovery_level()
+ */
+define('SIMPLEPIE_LOCATOR_REMOTE_EXTENSION', NamespacedSimplePie::SIMPLEPIE_LOCATOR_REMOTE_EXTENSION);
+
+/**
+ * Remote Feed Body Autodiscovery
+ * @see SimplePie::set_autodiscovery_level()
+ */
+define('SIMPLEPIE_LOCATOR_REMOTE_BODY', NamespacedSimplePie::SIMPLEPIE_LOCATOR_REMOTE_BODY);
+
+/**
+ * All Feed Autodiscovery
+ * @see SimplePie::set_autodiscovery_level()
+ */
+define('SIMPLEPIE_LOCATOR_ALL', NamespacedSimplePie::SIMPLEPIE_LOCATOR_ALL);
+
+/**
+ * No known feed type
+ */
+define('SIMPLEPIE_TYPE_NONE', NamespacedSimplePie::SIMPLEPIE_TYPE_NONE);
+
+/**
+ * RSS 0.90
+ */
+define('SIMPLEPIE_TYPE_RSS_090', NamespacedSimplePie::SIMPLEPIE_TYPE_RSS_090);
+
+/**
+ * RSS 0.91 (Netscape)
+ */
+define('SIMPLEPIE_TYPE_RSS_091_NETSCAPE', NamespacedSimplePie::SIMPLEPIE_TYPE_RSS_091_NETSCAPE);
+
+/**
+ * RSS 0.91 (Userland)
+ */
+define('SIMPLEPIE_TYPE_RSS_091_USERLAND', NamespacedSimplePie::SIMPLEPIE_TYPE_RSS_091_USERLAND);
+
+/**
+ * RSS 0.91 (both Netscape and Userland)
+ */
+define('SIMPLEPIE_TYPE_RSS_091', NamespacedSimplePie::SIMPLEPIE_TYPE_RSS_091);
+
+/**
+ * RSS 0.92
+ */
+define('SIMPLEPIE_TYPE_RSS_092', NamespacedSimplePie::SIMPLEPIE_TYPE_RSS_092);
+
+/**
+ * RSS 0.93
+ */
+define('SIMPLEPIE_TYPE_RSS_093', NamespacedSimplePie::SIMPLEPIE_TYPE_RSS_093);
+
+/**
+ * RSS 0.94
+ */
+define('SIMPLEPIE_TYPE_RSS_094', NamespacedSimplePie::SIMPLEPIE_TYPE_RSS_094);
+
+/**
+ * RSS 1.0
+ */
+define('SIMPLEPIE_TYPE_RSS_10', NamespacedSimplePie::SIMPLEPIE_TYPE_RSS_10);
+
+/**
+ * RSS 2.0
+ */
+define('SIMPLEPIE_TYPE_RSS_20', NamespacedSimplePie::SIMPLEPIE_TYPE_RSS_20);
+
+/**
+ * RDF-based RSS
+ */
+define('SIMPLEPIE_TYPE_RSS_RDF', NamespacedSimplePie::SIMPLEPIE_TYPE_RSS_RDF);
+
+/**
+ * Non-RDF-based RSS (truly intended as syndication format)
+ */
+define('SIMPLEPIE_TYPE_RSS_SYNDICATION', NamespacedSimplePie::SIMPLEPIE_TYPE_RSS_SYNDICATION);
+
+/**
+ * All RSS
+ */
+define('SIMPLEPIE_TYPE_RSS_ALL', NamespacedSimplePie::SIMPLEPIE_TYPE_RSS_ALL);
+
+/**
+ * Atom 0.3
+ */
+define('SIMPLEPIE_TYPE_ATOM_03', NamespacedSimplePie::SIMPLEPIE_TYPE_ATOM_03);
+
+/**
+ * Atom 1.0
+ */
+define('SIMPLEPIE_TYPE_ATOM_10', NamespacedSimplePie::SIMPLEPIE_TYPE_ATOM_10);
+
+/**
+ * All Atom
+ */
+define('SIMPLEPIE_TYPE_ATOM_ALL', NamespacedSimplePie::SIMPLEPIE_TYPE_ATOM_ALL);
+
+/**
+ * All feed types
+ */
+define('SIMPLEPIE_TYPE_ALL', NamespacedSimplePie::SIMPLEPIE_TYPE_ALL);
+
+/**
+ * No construct
+ */
+define('SIMPLEPIE_CONSTRUCT_NONE', NamespacedSimplePie::SIMPLEPIE_CONSTRUCT_NONE);
+
+/**
+ * Text construct
+ */
+define('SIMPLEPIE_CONSTRUCT_TEXT', NamespacedSimplePie::SIMPLEPIE_CONSTRUCT_TEXT);
+
+/**
+ * HTML construct
+ */
+define('SIMPLEPIE_CONSTRUCT_HTML', NamespacedSimplePie::SIMPLEPIE_CONSTRUCT_HTML);
+
+/**
+ * XHTML construct
+ */
+define('SIMPLEPIE_CONSTRUCT_XHTML', NamespacedSimplePie::SIMPLEPIE_CONSTRUCT_XHTML);
+
+/**
+ * base64-encoded construct
+ */
+define('SIMPLEPIE_CONSTRUCT_BASE64', NamespacedSimplePie::SIMPLEPIE_CONSTRUCT_BASE64);
+
+/**
+ * IRI construct
+ */
+define('SIMPLEPIE_CONSTRUCT_IRI', NamespacedSimplePie::SIMPLEPIE_CONSTRUCT_IRI);
+
+/**
+ * A construct that might be HTML
+ */
+define('SIMPLEPIE_CONSTRUCT_MAYBE_HTML', NamespacedSimplePie::SIMPLEPIE_CONSTRUCT_MAYBE_HTML);
+
+/**
+ * All constructs
+ */
+define('SIMPLEPIE_CONSTRUCT_ALL', NamespacedSimplePie::SIMPLEPIE_CONSTRUCT_ALL);
+
+/**
+ * Don't change case
+ */
+define('SIMPLEPIE_SAME_CASE', NamespacedSimplePie::SIMPLEPIE_SAME_CASE);
+
+/**
+ * Change to lowercase
+ */
+define('SIMPLEPIE_LOWERCASE', NamespacedSimplePie::SIMPLEPIE_LOWERCASE);
+
+/**
+ * Change to uppercase
+ */
+define('SIMPLEPIE_UPPERCASE', NamespacedSimplePie::SIMPLEPIE_UPPERCASE);
+
+/**
+ * PCRE for HTML attributes
+ */
+define('SIMPLEPIE_PCRE_HTML_ATTRIBUTE', NamespacedSimplePie::SIMPLEPIE_PCRE_HTML_ATTRIBUTE);
+
+/**
+ * PCRE for XML attributes
+ */
+define('SIMPLEPIE_PCRE_XML_ATTRIBUTE', NamespacedSimplePie::SIMPLEPIE_PCRE_XML_ATTRIBUTE);
+
+/**
+ * XML Namespace
+ */
+define('SIMPLEPIE_NAMESPACE_XML', NamespacedSimplePie::SIMPLEPIE_NAMESPACE_XML);
+
+/**
+ * Atom 1.0 Namespace
+ */
+define('SIMPLEPIE_NAMESPACE_ATOM_10', NamespacedSimplePie::SIMPLEPIE_NAMESPACE_ATOM_10);
+
+/**
+ * Atom 0.3 Namespace
+ */
+define('SIMPLEPIE_NAMESPACE_ATOM_03', NamespacedSimplePie::SIMPLEPIE_NAMESPACE_ATOM_03);
+
+/**
+ * RDF Namespace
+ */
+define('SIMPLEPIE_NAMESPACE_RDF', NamespacedSimplePie::SIMPLEPIE_NAMESPACE_RDF);
+
+/**
+ * RSS 0.90 Namespace
+ */
+define('SIMPLEPIE_NAMESPACE_RSS_090', NamespacedSimplePie::SIMPLEPIE_NAMESPACE_RSS_090);
+
+/**
+ * RSS 1.0 Namespace
+ */
+define('SIMPLEPIE_NAMESPACE_RSS_10', NamespacedSimplePie::SIMPLEPIE_NAMESPACE_RSS_10);
+
+/**
+ * RSS 1.0 Content Module Namespace
+ */
+define('SIMPLEPIE_NAMESPACE_RSS_10_MODULES_CONTENT', NamespacedSimplePie::SIMPLEPIE_NAMESPACE_RSS_10_MODULES_CONTENT);
+
+/**
+ * RSS 2.0 Namespace
+ * (Stupid, I know, but I'm certain it will confuse people less with support.)
+ */
+define('SIMPLEPIE_NAMESPACE_RSS_20', NamespacedSimplePie::SIMPLEPIE_NAMESPACE_RSS_20);
+
+/**
+ * DC 1.0 Namespace
+ */
+define('SIMPLEPIE_NAMESPACE_DC_10', NamespacedSimplePie::SIMPLEPIE_NAMESPACE_DC_10);
+
+/**
+ * DC 1.1 Namespace
+ */
+define('SIMPLEPIE_NAMESPACE_DC_11', NamespacedSimplePie::SIMPLEPIE_NAMESPACE_DC_11);
+
+/**
+ * W3C Basic Geo (WGS84 lat/long) Vocabulary Namespace
+ */
+define('SIMPLEPIE_NAMESPACE_W3C_BASIC_GEO', NamespacedSimplePie::SIMPLEPIE_NAMESPACE_W3C_BASIC_GEO);
+
+/**
+ * GeoRSS Namespace
+ */
+define('SIMPLEPIE_NAMESPACE_GEORSS', NamespacedSimplePie::SIMPLEPIE_NAMESPACE_GEORSS);
+
+/**
+ * Media RSS Namespace
+ */
+define('SIMPLEPIE_NAMESPACE_MEDIARSS', NamespacedSimplePie::SIMPLEPIE_NAMESPACE_MEDIARSS);
+
+/**
+ * Wrong Media RSS Namespace. Caused by a long-standing typo in the spec.
+ */
+define('SIMPLEPIE_NAMESPACE_MEDIARSS_WRONG', NamespacedSimplePie::SIMPLEPIE_NAMESPACE_MEDIARSS_WRONG);
+
+/**
+ * Wrong Media RSS Namespace #2. New namespace introduced in Media RSS 1.5.
+ */
+define('SIMPLEPIE_NAMESPACE_MEDIARSS_WRONG2', NamespacedSimplePie::SIMPLEPIE_NAMESPACE_MEDIARSS_WRONG2);
+
+/**
+ * Wrong Media RSS Namespace #3. A possible typo of the Media RSS 1.5 namespace.
+ */
+define('SIMPLEPIE_NAMESPACE_MEDIARSS_WRONG3', NamespacedSimplePie::SIMPLEPIE_NAMESPACE_MEDIARSS_WRONG3);
+
+/**
+ * Wrong Media RSS Namespace #4. New spec location after the RSS Advisory Board takes it over, but not a valid namespace.
+ */
+define('SIMPLEPIE_NAMESPACE_MEDIARSS_WRONG4', NamespacedSimplePie::SIMPLEPIE_NAMESPACE_MEDIARSS_WRONG4);
+
+/**
+ * Wrong Media RSS Namespace #5. A possible typo of the RSS Advisory Board URL.
+ */
+define('SIMPLEPIE_NAMESPACE_MEDIARSS_WRONG5', NamespacedSimplePie::SIMPLEPIE_NAMESPACE_MEDIARSS_WRONG5);
+
+/**
+ * iTunes RSS Namespace
+ */
+define('SIMPLEPIE_NAMESPACE_ITUNES', NamespacedSimplePie::SIMPLEPIE_NAMESPACE_ITUNES);
+
+/**
+ * XHTML Namespace
+ */
+define('SIMPLEPIE_NAMESPACE_XHTML', NamespacedSimplePie::SIMPLEPIE_NAMESPACE_XHTML);
+
+/**
+ * IANA Link Relations Registry
+ */
+define('SIMPLEPIE_IANA_LINK_RELATIONS_REGISTRY', NamespacedSimplePie::SIMPLEPIE_IANA_LINK_RELATIONS_REGISTRY);
+
+/**
+ * No file source
+ */
+define('SIMPLEPIE_FILE_SOURCE_NONE', NamespacedSimplePie::SIMPLEPIE_FILE_SOURCE_NONE);
+
+/**
+ * Remote file source
+ */
+define('SIMPLEPIE_FILE_SOURCE_REMOTE', NamespacedSimplePie::SIMPLEPIE_FILE_SOURCE_REMOTE);
+
+/**
+ * Local file source
+ */
+define('SIMPLEPIE_FILE_SOURCE_LOCAL', NamespacedSimplePie::SIMPLEPIE_FILE_SOURCE_LOCAL);
+
+/**
+ * fsockopen() file source
+ */
+define('SIMPLEPIE_FILE_SOURCE_FSOCKOPEN', NamespacedSimplePie::SIMPLEPIE_FILE_SOURCE_FSOCKOPEN);
+
+/**
+ * cURL file source
+ */
+define('SIMPLEPIE_FILE_SOURCE_CURL', NamespacedSimplePie::SIMPLEPIE_FILE_SOURCE_CURL);
+
+/**
+ * file_get_contents() file source
+ */
+define('SIMPLEPIE_FILE_SOURCE_FILE_GET_CONTENTS', NamespacedSimplePie::SIMPLEPIE_FILE_SOURCE_FILE_GET_CONTENTS);
--- src/SimplePie.php
+++ src/SimplePie.php
@@ -55,6 +55,366 @@ namespace SimplePie;
 class SimplePie
 {
 	/**
+	 * SimplePie Name
+	 */
+	public const SIMPLEPIE_NAME = 'SimplePie';
+
+	/**
+	 * SimplePie Version
+	 */
+	public const SIMPLEPIE_VERSION = '1.5.8';
+
+	/**
+	 * SimplePie Build
+	 * @todo using a function is an invalid constant expression. Maybe we should deprecate SIMPLEPIE_BUILD?
+	 */
+	// public const SIMPLEPIE_BUILD = gmdate('YmdHis', \SimplePie\Misc::get_build());
+
+	/**
+	 * SimplePie Website URL
+	 */
+	public const SIMPLEPIE_URL = 'http://simplepie.org';
+
+	/**
+	 * SimplePie Useragent
+	 * @see SimplePie::set_useragent()
+	 */
+	public const SIMPLEPIE_USERAGENT = SIMPLEPIE_NAME . '/' . SIMPLEPIE_VERSION . ' (Feed Parser; ' . SIMPLEPIE_URL . '; Allow like Gecko) Build/' . SIMPLEPIE_BUILD;
+
+	/**
+	 * SimplePie Linkback
+	 */
+	public const SIMPLEPIE_LINKBACK = '<a href="' . SIMPLEPIE_URL . '" title="' . SIMPLEPIE_NAME . ' ' . SIMPLEPIE_VERSION . '">' . SIMPLEPIE_NAME . '</a>';
+
+	/**
+	 * No Autodiscovery
+	 * @see SimplePie::set_autodiscovery_level()
+	 */
+	public const SIMPLEPIE_LOCATOR_NONE = 0;
+
+	/**
+	 * Feed Link Element Autodiscovery
+	 * @see SimplePie::set_autodiscovery_level()
+	 */
+	public const SIMPLEPIE_LOCATOR_AUTODISCOVERY = 1;
+
+	/**
+	 * Local Feed Extension Autodiscovery
+	 * @see SimplePie::set_autodiscovery_level()
+	 */
+	public const SIMPLEPIE_LOCATOR_LOCAL_EXTENSION = 2;
+
+	/**
+	 * Local Feed Body Autodiscovery
+	 * @see SimplePie::set_autodiscovery_level()
+	 */
+	public const SIMPLEPIE_LOCATOR_LOCAL_BODY = 4;
+
+	/**
+	 * Remote Feed Extension Autodiscovery
+	 * @see SimplePie::set_autodiscovery_level()
+	 */
+	public const SIMPLEPIE_LOCATOR_REMOTE_EXTENSION = 8;
+
+	/**
+	 * Remote Feed Body Autodiscovery
+	 * @see SimplePie::set_autodiscovery_level()
+	 */
+	public const SIMPLEPIE_LOCATOR_REMOTE_BODY = 16;
+
+	/**
+	 * All Feed Autodiscovery
+	 * @see SimplePie::set_autodiscovery_level()
+	 */
+	public const SIMPLEPIE_LOCATOR_ALL = 31;
+
+	/**
+	 * No known feed type
+	 */
+	public const SIMPLEPIE_TYPE_NONE = 0;
+
+	/**
+	 * RSS 0.90
+	 */
+	public const SIMPLEPIE_TYPE_RSS_090 = 1;
+
+	/**
+	 * RSS 0.91 (Netscape)
+	 */
+	public const SIMPLEPIE_TYPE_RSS_091_NETSCAPE = 2;
+
+	/**
+	 * RSS 0.91 (Userland)
+	 */
+	public const SIMPLEPIE_TYPE_RSS_091_USERLAND = 4;
+
+	/**
+	 * RSS 0.91 (both Netscape and Userland)
+	 */
+	public const SIMPLEPIE_TYPE_RSS_091 = 6;
+
+	/**
+	 * RSS 0.92
+	 */
+	public const SIMPLEPIE_TYPE_RSS_092 = 8;
+
+	/**
+	 * RSS 0.93
+	 */
+	public const SIMPLEPIE_TYPE_RSS_093 = 16;
+
+	/**
+	 * RSS 0.94
+	 */
+	public const SIMPLEPIE_TYPE_RSS_094 = 32;
+
+	/**
+	 * RSS 1.0
+	 */
+	public const SIMPLEPIE_TYPE_RSS_10 = 64;
+
+	/**
+	 * RSS 2.0
+	 */
+	public const SIMPLEPIE_TYPE_RSS_20 = 128;
+
+	/**
+	 * RDF-based RSS
+	 */
+	public const SIMPLEPIE_TYPE_RSS_RDF = 65;
+
+	/**
+	 * Non-RDF-based RSS (truly intended as syndication format)
+	 */
+	public const SIMPLEPIE_TYPE_RSS_SYNDICATION = 190;
+
+	/**
+	 * All RSS
+	 */
+	public const SIMPLEPIE_TYPE_RSS_ALL = 255;
+
+	/**
+	 * Atom 0.3
+	 */
+	public const SIMPLEPIE_TYPE_ATOM_03 = 256;
+
+	/**
+	 * Atom 1.0
+	 */
+	public const SIMPLEPIE_TYPE_ATOM_10 = 512;
+
+	/**
+	 * All Atom
+	 */
+	public const SIMPLEPIE_TYPE_ATOM_ALL = 768;
+
+	/**
+	 * All feed types
+	 */
+	public const SIMPLEPIE_TYPE_ALL = 1023;
+
+	/**
+	 * No construct
+	 */
+	public const SIMPLEPIE_CONSTRUCT_NONE = 0;
+
+	/**
+	 * Text construct
+	 */
+	public const SIMPLEPIE_CONSTRUCT_TEXT = 1;
+
+	/**
+	 * HTML construct
+	 */
+	public const SIMPLEPIE_CONSTRUCT_HTML = 2;
+
+	/**
+	 * XHTML construct
+	 */
+	public const SIMPLEPIE_CONSTRUCT_XHTML = 4;
+
+	/**
+	 * base64-encoded construct
+	 */
+	public const SIMPLEPIE_CONSTRUCT_BASE64 = 8;
+
+	/**
+	 * IRI construct
+	 */
+	public const SIMPLEPIE_CONSTRUCT_IRI = 16;
+
+	/**
+	 * A construct that might be HTML
+	 */
+	public const SIMPLEPIE_CONSTRUCT_MAYBE_HTML = 32;
+
+	/**
+	 * All constructs
+	 */
+	public const SIMPLEPIE_CONSTRUCT_ALL = 63;
+
+	/**
+	 * Don't change case
+	 */
+	public const SIMPLEPIE_SAME_CASE = 1;
+
+	/**
+	 * Change to lowercase
+	 */
+	public const SIMPLEPIE_LOWERCASE = 2;
+
+	/**
+	 * Change to uppercase
+	 */
+	public const SIMPLEPIE_UPPERCASE = 4;
+
+	/**
+	 * PCRE for HTML attributes
+	 */
+	public const SIMPLEPIE_PCRE_HTML_ATTRIBUTE = '((?:[\x09\x0A\x0B\x0C\x0D\x20]+[^\x09\x0A\x0B\x0C\x0D\x20\x2F\x3E][^\x09\x0A\x0B\x0C\x0D\x20\x2F\x3D\x3E]*(?:[\x09\x0A\x0B\x0C\x0D\x20]*=[\x09\x0A\x0B\x0C\x0D\x20]*(?:"(?:[^"]*)"|\'(?:[^\']*)\'|(?:[^\x09\x0A\x0B\x0C\x0D\x20\x22\x27\x3E][^\x09\x0A\x0B\x0C\x0D\x20\x3E]*)?))?)*)[\x09\x0A\x0B\x0C\x0D\x20]*';
+
+	/**
+	 * PCRE for XML attributes
+	 */
+	public const SIMPLEPIE_PCRE_XML_ATTRIBUTE = '((?:\s+(?:(?:[^\s:]+:)?[^\s:]+)\s*=\s*(?:"(?:[^"]*)"|\'(?:[^\']*)\'))*)\s*';
+
+	/**
+	 * XML Namespace
+	 */
+	public const SIMPLEPIE_NAMESPACE_XML = 'http://www.w3.org/XML/1998/namespace';
+
+	/**
+	 * Atom 1.0 Namespace
+	 */
+	public const SIMPLEPIE_NAMESPACE_ATOM_10 = 'http://www.w3.org/2005/Atom';
+
+	/**
+	 * Atom 0.3 Namespace
+	 */
+	public const SIMPLEPIE_NAMESPACE_ATOM_03 = 'http://purl.org/atom/ns#';
+
+	/**
+	 * RDF Namespace
+	 */
+	public const SIMPLEPIE_NAMESPACE_RDF = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#';
+
+	/**
+	 * RSS 0.90 Namespace
+	 */
+	public const SIMPLEPIE_NAMESPACE_RSS_090 = 'http://my.netscape.com/rdf/simple/0.9/';
+
+	/**
+	 * RSS 1.0 Namespace
+	 */
+	public const SIMPLEPIE_NAMESPACE_RSS_10 = 'http://purl.org/rss/1.0/';
+
+	/**
+	 * RSS 1.0 Content Module Namespace
+	 */
+	public const SIMPLEPIE_NAMESPACE_RSS_10_MODULES_CONTENT = 'http://purl.org/rss/1.0/modules/content/';
+
+	/**
+	 * RSS 2.0 Namespace
+	 * (Stupid, I know, but I'm certain it will confuse people less with support.)
+	 */
+	public const SIMPLEPIE_NAMESPACE_RSS_20 = '';
+
+	/**
+	 * DC 1.0 Namespace
+	 */
+	public const SIMPLEPIE_NAMESPACE_DC_10 = 'http://purl.org/dc/elements/1.0/';
+
+	/**
+	 * DC 1.1 Namespace
+	 */
+	public const SIMPLEPIE_NAMESPACE_DC_11 = 'http://purl.org/dc/elements/1.1/';
+
+	/**
+	 * W3C Basic Geo (WGS84 lat/long) Vocabulary Namespace
+	 */
+	public const SIMPLEPIE_NAMESPACE_W3C_BASIC_GEO = 'http://www.w3.org/2003/01/geo/wgs84_pos#';
+
+	/**
+	 * GeoRSS Namespace
+	 */
+	public const SIMPLEPIE_NAMESPACE_GEORSS = 'http://www.georss.org/georss';
+
+	/**
+	 * Media RSS Namespace
+	 */
+	public const SIMPLEPIE_NAMESPACE_MEDIARSS = 'http://search.yahoo.com/mrss/';
+
+	/**
+	 * Wrong Media RSS Namespace. Caused by a long-standing typo in the spec.
+	 */
+	public const SIMPLEPIE_NAMESPACE_MEDIARSS_WRONG = 'http://search.yahoo.com/mrss';
+
+	/**
+	 * Wrong Media RSS Namespace #2. New namespace introduced in Media RSS 1.5.
+	 */
+	public const SIMPLEPIE_NAMESPACE_MEDIARSS_WRONG2 = 'http://video.search.yahoo.com/mrss';
+
+	/**
+	 * Wrong Media RSS Namespace #3. A possible typo of the Media RSS 1.5 namespace.
+	 */
+	public const SIMPLEPIE_NAMESPACE_MEDIARSS_WRONG3 = 'http://video.search.yahoo.com/mrss/';
+
+	/**
+	 * Wrong Media RSS Namespace #4. New spec location after the RSS Advisory Board takes it over, but not a valid namespace.
+	 */
+	public const SIMPLEPIE_NAMESPACE_MEDIARSS_WRONG4 = 'http://www.rssboard.org/media-rss';
+
+	/**
+	 * Wrong Media RSS Namespace #5. A possible typo of the RSS Advisory Board URL.
+	 */
+	public const SIMPLEPIE_NAMESPACE_MEDIARSS_WRONG5 = 'http://www.rssboard.org/media-rss/';
+
+	/**
+	 * iTunes RSS Namespace
+	 */
+	public const SIMPLEPIE_NAMESPACE_ITUNES = 'http://www.itunes.com/dtds/podcast-1.0.dtd';
+
+	/**
+	 * XHTML Namespace
+	 */
+	public const SIMPLEPIE_NAMESPACE_XHTML = 'http://www.w3.org/1999/xhtml';
+
+	/**
+	 * IANA Link Relations Registry
+	 */
+	public const SIMPLEPIE_IANA_LINK_RELATIONS_REGISTRY = 'http://www.iana.org/assignments/relation/';
+
+	/**
+	 * No file source
+	 */
+	public const SIMPLEPIE_FILE_SOURCE_NONE = 0;
+
+	/**
+	 * Remote file source
+	 */
+	public const SIMPLEPIE_FILE_SOURCE_REMOTE = 1;
+
+	/**
+	 * Local file source
+	 */
+	public const SIMPLEPIE_FILE_SOURCE_LOCAL = 2;
+
+	/**
+	 * fsockopen() file source
+	 */
+	public const SIMPLEPIE_FILE_SOURCE_FSOCKOPEN = 4;
+
+	/**
+	 * cURL file source
+	 */
+	public const SIMPLEPIE_FILE_SOURCE_CURL = 8;
+
+	/**
+	 * file_get_contents() file source
+	 */
+	public const SIMPLEPIE_FILE_SOURCE_FILE_GET_CONTENTS = 16;
+
+	/**
 	 * @var array Raw data
 	 * @access private
 	 */
