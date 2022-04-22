73188dbb1c8f42255457859b2d8c713da00cdcaf
Art4 <art4@wlabs.de>
2022-04-22 15:41:32 +0200

    Update year in copyright note

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -5,7 +5,7 @@
  * A PHP-Based RSS and Atom Feed Framework.
  * Takes the hard work out of managing a complete RSS/Atom solution.
  *
- * Copyright (c) 2004-2016, Ryan Parman, Sam Sneddon, Ryan McCue, and contributors
+ * Copyright (c) 2004-2022, Ryan Parman, Sam Sneddon, Ryan McCue, and contributors
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are
@@ -48,7 +48,7 @@ require_once dirname(__FILE__) . '/IRITest.php';
 require_once dirname(__FILE__) . '/LocatorTest.php';
 require_once dirname(__FILE__) . '/ItemTest.php';
 require_once dirname(__FILE__) . '/oldtests.php';
- 
+
 class AllTests
 {
 	public static function suite()
@@ -64,7 +64,7 @@ class AllTests
 		$suite->addTestSuite('ItemTest');
 		$suite->addTestSuite('OldTest');
 		$suite->addTestSuite('SubscribeUrlTest');
- 
+
 		return $suite;
 	}
 }
