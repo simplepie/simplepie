9a7be6282972e90eb3909877002dd655be8b57b9
Art4 <art4@wlabs.de>
2022-04-25 13:59:13 +0200

    Fix tests with compiled file

--- build/bootstrap.php
+++ build/bootstrap.php
@@ -2,4 +2,4 @@
 
 require_once dirname(__DIR__) . '/SimplePie.compiled.php';
 require_once dirname(__DIR__) . '/vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
-require_once dirname(__DIR__) . '/tests/Fixtures/Mocks.php';
+require_once dirname(__DIR__) . '/tests/Mocks.php';
