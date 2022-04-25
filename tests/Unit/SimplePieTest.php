3d49287a0043078e1b9a17c2881e64ba59df4bf7
Art4 <art4@wlabs.de>
2022-04-25 16:14:11 +0200

    Implement tests from HTTPParserTest, IRITest and ItemTest.php

--- tests/Unit/SimplePieTest.php
+++ tests/Unit/SimplePieTest.php
@@ -64,6 +64,183 @@ class SimplePieTest extends TestCase
 		$this->assertTrue(class_exists('SimplePie'));
 	}
 
+	/**
+	 * Run a test using a sprintf template and data
+	 *
+	 * @param string $template
+	 */
+	private function createFeedWithTemplate($template, $data)
+	{
+		if (!is_array($data))
+		{
+			$data = array($data);
+		}
+		$xml = vsprintf($template, $data);
+		$feed = new SimplePie();
+		$feed->set_raw_data($xml);
+		$feed->enable_cache(false);
+		$feed->init();
+
+		return $feed;
+	}
+
+	public static function titleDataProvider()
+	{
+		return array(
+			array('Feed Title', 'Feed Title'),
+
+			// RSS Profile tests
+			array('AT&amp;T', 'AT&amp;T'),
+			array('AT&#x26;T', 'AT&amp;T'),
+			array("Bill &amp; Ted's Excellent Adventure", "Bill &amp; Ted's Excellent Adventure"),
+			array("Bill &#x26; Ted's Excellent Adventure", "Bill &amp; Ted's Excellent Adventure"),
+			array('The &amp; entity', 'The &amp; entity'),
+			array('The &#x26; entity', 'The &amp; entity'),
+			array('The &amp;amp; entity', 'The &amp;amp; entity'),
+			array('The &#x26;amp; entity', 'The &amp;amp; entity'),
+			array('I &lt;3 Phil Ringnalda', 'I &lt;3 Phil Ringnalda'),
+			array('I &#x3C;3 Phil Ringnalda', 'I &lt;3 Phil Ringnalda'),
+			array('A &lt; B', 'A &lt; B'),
+			array('A &#x3C; B', 'A &lt; B'),
+			array('A&lt;B', 'A&lt;B'),
+			array('A&#x3C;B', 'A&lt;B'),
+			array("Nice &lt;gorilla&gt; what's he weigh?", "Nice &lt;gorilla&gt; what's he weigh?"),
+			array("Nice &#x3C;gorilla&gt; what's he weigh?", "Nice &lt;gorilla&gt; what's he weigh?"),
+		);
+	}
+
+	/**
+	 * @dataProvider titleDataProvider
+	 */
+	public function testTitleRSS20($title, $expected)
+	{
+		$data =
+'<rss version="2.0">
+	<channel>
+		<title>%s</title>
+	</channel>
+</rss>';
+		$feed = $this->createFeedWithTemplate($data, $title);
+		$this->assertSame($expected, $feed->get_title());
+	}
+
+	/**
+	 * @dataProvider titleDataProvider
+	 */
+	public function testTitleRSS20WithDC10($title, $expected)
+	{
+		$data =
+'<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.0/">
+	<channel>
+		<dc:title>%s</dc:title>
+	</channel>
+</rss>';
+		$feed = $this->createFeedWithTemplate($data, $title);
+		$this->assertSame($expected, $feed->get_title());
+	}
+
+	/**
+	 * @dataProvider titleDataProvider
+	 */
+	public function testTitleRSS20WithDC11($title, $expected)
+	{
+		$data =
+'<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/">
+	<channel>
+		<dc:title>%s</dc:title>
+	</channel>
+</rss>';
+		$feed = $this->createFeedWithTemplate($data, $title);
+		$this->assertSame($expected, $feed->get_title());
+	}
+
+	/**
+	 * @dataProvider titleDataProvider
+	 */
+	public function testTitleRSS20WithAtom03($title, $expected)
+	{
+		$data =
+'<rss version="2.0" xmlns:a="http://purl.org/atom/ns#">
+	<channel>
+		<a:title>%s</a:title>
+	</channel>
+</rss>';
+		$feed = $this->createFeedWithTemplate($data, $title);
+		$this->assertSame($expected, $feed->get_title());
+	}
+
+	/**
+	 * @dataProvider titleDataProvider
+	 */
+	public function testTitleRSS20WithAtom10($title, $expected)
+	{
+		$data =
+'<rss version="2.0" xmlns:a="http://www.w3.org/2005/Atom">
+	<channel>
+		<a:title>%s</a:title>
+	</channel>
+</rss>';
+		$feed = $this->createFeedWithTemplate($data, $title);
+		$this->assertSame($expected, $feed->get_title());
+	}
+
+	/**
+	 * Based on a test from old bug 18
+	 *
+	 * @dataProvider titleDataProvider
+	 */
+	public function testTitleRSS20WithImageTitle($title, $expected)
+	{
+		$data =
+'<rss version="2.0">
+	<channel>
+		<title>%s</title>
+		<image>
+			<title>Image title</title>
+		</image>
+	</channel>
+</rss>';
+		$feed = $this->createFeedWithTemplate($data, $title);
+		$this->assertSame($expected, $feed->get_title());
+	}
+
+	/**
+	 * Based on a test from old bug 18
+	 *
+	 * @dataProvider titleDataProvider
+	 */
+	public function testTitleRSS20WithImageTitleReversed($title, $expected)
+	{
+		$data =
+'<rss version="2.0">
+	<channel>
+		<image>
+			<title>Image title</title>
+		</image>
+		<title>%s</title>
+	</channel>
+</rss>';
+		$feed = $this->createFeedWithTemplate($data, $title);
+		$this->assertSame($expected, $feed->get_title());
+	}
+
+	public function testItemWithEmptyContent()
+	{
+		$data =
+'<rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/">
+	<channel>
+		<item>
+			<description>%s</description>
+			<content:encoded><![CDATA[ <script> ]]></content:encoded>
+		</item>
+	</channel>
+</rss>';
+		$content = 'item description';
+		$feed = $this->createFeedWithTemplate($data, $content);
+		$item = $feed->get_item();
+		$this->assertSame($content, $item->get_content());
+	}
+
 	public function testLegacyCallOfSetCacheClass()
 	{
 		if (PHP_VERSION_ID < 80000) {
