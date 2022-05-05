1d3855331dd651b26e8c3ac095408866ef91b5f2
Art4 <art4@wlabs.de>
2022-05-05 08:51:19 +0200

    Fix tests

--- tests/Unit/SimplePieTest.php
+++ tests/Unit/SimplePieTest.php
@@ -1894,7 +1894,6 @@ EOT
 				,
 				NULL,
 			],
-	}
 			'Test Atom 1.0 Logo Default' => [
 <<<EOT
 <feed xmlns="http://www.w3.org/2005/Atom">
@@ -1904,7 +1903,6 @@ EOT
 				,
 				NULL,
 			],
-	}
 			'Test RSS 0.90 Atom 1.0 Icon' => [
 <<<EOT
 <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns="http://my.netscape.com/rdf/simple/0.9/" xmlns:a="http://www.w3.org/2005/Atom">
