078078f431f47166975c53ce81d68ab4285fc1f0
Art4 <art4@wlabs.de>
2022-08-29 10:47:48 +0200

    Create wrapper for PSR-16 cache

---
+++ src/Cache/Psr16.php
@@ -0,0 +1,204 @@
+<?php
+/**
+ * SimplePie
+ *
+ * A PHP-Based RSS and Atom Feed Framework.
+ * Takes the hard work out of managing a complete RSS/Atom solution.
+ *
+ * Copyright (c) 2004-2022, Ryan Parman, Sam Sneddon, Ryan McCue, and contributors
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ * 	* Redistributions of source code must retain the above copyright notice, this list of
+ * 	  conditions and the following disclaimer.
+ *
+ * 	* Redistributions in binary form must reproduce the above copyright notice, this list
+ * 	  of conditions and the following disclaimer in the documentation and/or other materials
+ * 	  provided with the distribution.
+ *
+ * 	* Neither the name of the SimplePie Team nor the names of its contributors may be used
+ * 	  to endorse or promote products derived from this software without specific prior
+ * 	  written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS
+ * AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package SimplePie
+ * @copyright 2004-2016 Ryan Parman, Sam Sneddon, Ryan McCue
+ * @author Ryan Parman
+ * @author Sam Sneddon
+ * @author Ryan McCue
+ * @link http://simplepie.org/ SimplePie
+ * @license http://www.opensource.org/licenses/bsd-license.php BSD License
+ */
+
+namespace SimplePie\Cache;
+
+use Exception;
+use Psr\SimpleCache\CacheException;
+use Psr\SimpleCache\CacheInterface;
+use SimplePie\SimplePie as SimplePieInstance;
+
+/**
+ * Caches data to the PSR-16 cache implementation
+ *
+ * @package SimplePie
+ * @subpackage Caching
+ */
+class Psr16 implements Base
+{
+    /**
+     * PSR-16 cache implemtentation
+     *
+     * @var CacheInterface
+     */
+    private $psr16cache;
+
+    /**
+     * PSR-16 cache key
+     *
+     * @var string
+     */
+    private $cacheKey;
+
+    /**
+     * Create a new cache object
+     *
+     * @param string $location Location string (from SimplePie::$cache_location)
+     * @param string $name Unique ID for the cache
+     * @param string $type Either TYPE_FEED for SimplePie data, or TYPE_IMAGE for image data
+     * @param CacheInterface $psr16cache
+     */
+    public function __construct($location, $name, $type/* , CacheInterface $psr16cache*/)
+    {
+        if (4 !== func_num_args()) {
+            throw new Exception(sprintf(
+                'Too few arguments to function %s, 3 passed, but exactly 4 expected.',
+                __METHOD__
+            ), \E_USER_ERROR);
+        }
+
+        $psr16cache = func_get_arg(3);
+
+        if (! $psr16cache instanceof CacheInterface) {
+            throw new Exception(sprintf(
+                'Argument #4 ($psr16cache) in %s must be of type Psr\SimpleCache\CacheInterface.',
+                __METHOD__
+            ), \E_USER_ERROR);
+        }
+
+        $this->psr16cache = $psr16cache;
+
+        // BC: hashAlgo sha256 support was added in PHP 7.1
+        $hashAlgo = in_array('sha256', hash_algos()) ? 'sha256' : 'sha1';
+        $this->cacheKey = hash($hashAlgo, "$location/$name.$type");
+    }
+
+    /**
+     * Save data to the cache
+     *
+     * @param array|SimplePieInstance $data Data to store in the cache. If passed a SimplePie object, only cache the $data property
+     * @return bool Successfulness
+     */
+    public function save($data)
+    {
+        if ($data instanceof SimplePieInstance) {
+            $data = $data->data;
+        }
+
+        try {
+            $this->psr16cache->set($this->cacheKey, serialize($data));
+        } catch (CacheException $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Retrieve the data saved to the cache
+     *
+     * @return array Data for SimplePie::$data
+     */
+    public function load()
+    {
+        try {
+            $data = $this->psr16cache->get($this->cacheKey, $this);
+
+            if ($data === $this) {
+                return false;
+            }
+        } catch (CacheException $th) {
+            return false;
+        }
+
+        return unserialize($data);
+    }
+
+    /**
+     * Retrieve the last modified time for the cache
+     *
+     * @return int Timestamp
+     */
+    public function mtime()
+    {
+        try {
+            if ($this->psr16cache->get($this->cacheKey, $this) === $this) {
+                return 0;
+            }
+        } catch (CacheException $th) {
+            return 0;
+        }
+
+        return time();
+    }
+
+    /**
+     * Set the last modified time to the current time
+     *
+     * @return bool Success status
+     */
+    public function touch()
+    {
+        try {
+            $data = $this->psr16cache->get($this->cacheKey, $this);
+
+            if ($data === $this) {
+                return false;
+            }
+
+            $this->psr16cache->delete($this->cacheKey);
+
+            $this->psr16cache->set($this->cacheKey, $data);
+        } catch (CacheException $th) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Remove the cache
+     *
+     * @return bool Success status
+     */
+    public function unlink()
+    {
+        try {
+            $this->psr16cache->delete($this->cacheKey);
+        } catch (CacheException $th) {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ tests/Unit/Cache/Psr16Test.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * SimplePie
+ *
+ * A PHP-Based RSS and Atom Feed Framework.
+ * Takes the hard work out of managing a complete RSS/Atom solution.
+ *
+ * Copyright (c) 2004-2022, Ryan Parman, Sam Sneddon, Ryan McCue, and contributors
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ * 	* Redistributions of source code must retain the above copyright notice, this list of
+ * 	  conditions and the following disclaimer.
+ *
+ * 	* Redistributions in binary form must reproduce the above copyright notice, this list
+ * 	  of conditions and the following disclaimer in the documentation and/or other materials
+ * 	  provided with the distribution.
+ *
+ * 	* Neither the name of the SimplePie Team nor the names of its contributors may be used
+ * 	  to endorse or promote products derived from this software without specific prior
+ * 	  written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS
+ * AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package SimplePie
+ * @copyright 2004-2022 Ryan Parman, Sam Sneddon, Ryan McCue
+ * @author Ryan Parman
+ * @author Sam Sneddon
+ * @author Ryan McCue
+ * @link http://simplepie.org/ SimplePie
+ * @license http://www.opensource.org/licenses/bsd-license.php BSD License
+ */
+
+namespace SimplePie\Tests\Unit\Cache;
+
+use Exception;
+use PHPUnit\Framework\TestCase;
+use Psr\SimpleCache\CacheInterface;
+use SimplePie\Cache\Psr16;
+
+class Psr16Test extends TestCase
+{
+    public function testConstructorArgument4IsPassed()
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Too few arguments to function SimplePie\Cache\Psr16::__construct, 3 passed, but exactly 4 expected.');
+
+        new Psr16('location', 'name', 'type');
+    }
+
+    public function testConstructorArgument4IsCacheInterface()
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Argument #4 ($psr16cache) in SimplePie\Cache\Psr16::__construct must be of type Psr\SimpleCache\CacheInterface.');
+
+        new Psr16('location', 'name', 'type', 'wrong type');
+    }
+}
