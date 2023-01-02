dfaf9ebbbc5a02e635494f17578a91e89e527856
Alexandre Alapetite <alexandre@alapetite.fr>
2023-01-02 10:53:37 +0100

    Update tests syntax

--- tests/Unit/EnclosureTest.php
+++ tests/Unit/EnclosureTest.php
@@ -141,51 +141,50 @@ XML
 
     public function getEnclosuresProvider()
     {
-        return [
-            'Test multiple enclosures MRSS' => [
-<<<EOT
-<rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/">
-    <channel>
-        <title>Test multiple enclosures MRSS</title>
-        <description>Test multiple enclosures MRSS</description>
-        <link>http://example.net/tests/</link>
-        <item>
-            <title>Test multiple enclosures MRSS 1</title>
-            <description>Test multiple enclosures MRSS 1</description>
-            <guid>http://example.net/tests/#1.1</guid>
-            <link>http://example.net/tests/#1.1</link>
-            <media:content url="http://example.net/a.jpg" medium="image">
-            </media:content>
-            <media:content url="http://example.net/b.jpg" medium="image">
-            </media:content>
-        </item>
-    </channel>
-</rss>
-EOT
-                ,
-                2,
-            ],
-            'Test multiple enclosures RSS2' => [
-<<<EOT
-<rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/">
-    <channel>
-        <title>Test multiple enclosures RSS2</title>
-        <description>Test multiple enclosures RSS2</description>
-        <link>http://example.net/tests/</link>
-        <item>
-            <title>Test multiple enclosures RSS2 1</title>
-            <description>Test multiple enclosures RSS2 1</description>
-            <guid>http://example.net/tests/#2.1</guid>
-            <link>http://example.net/tests/#2.1</link>
-            <enclosure url="http://example.net/a.jpg" type="image/jpeg"/>
-            <enclosure url="http://example.net/b.jpg" type="image/jpeg"/>
-        </item>
-    </channel>
-</rss>
-EOT
-                ,
-                2,
-            ],
+        yield 'Test multiple enclosures MRSS' => [
+            <<<XML
+            <rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/">
+                <channel>
+                    <title>Test multiple enclosures MRSS</title>
+                    <description>Test multiple enclosures MRSS</description>
+                    <link>http://example.net/tests/</link>
+                    <item>
+                        <title>Test multiple enclosures MRSS 1</title>
+                        <description>Test multiple enclosures MRSS 1</description>
+                        <guid>http://example.net/tests/#1.1</guid>
+                        <link>http://example.net/tests/#1.1</link>
+                        <media:content url="http://example.net/a.jpg" medium="image">
+                        </media:content>
+                        <media:content url="http://example.net/b.jpg" medium="image">
+                        </media:content>
+                    </item>
+                </channel>
+            </rss>
+XML
+            ,
+            2,
+        ];
+
+        yield 'Test multiple enclosures RSS2' => [
+            <<<XML
+            <rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/">
+                <channel>
+                    <title>Test multiple enclosures RSS2</title>
+                    <description>Test multiple enclosures RSS2</description>
+                    <link>http://example.net/tests/</link>
+                    <item>
+                        <title>Test multiple enclosures RSS2 1</title>
+                        <description>Test multiple enclosures RSS2 1</description>
+                        <guid>http://example.net/tests/#2.1</guid>
+                        <link>http://example.net/tests/#2.1</link>
+                        <enclosure url="http://example.net/a.jpg" type="image/jpeg"/>
+                        <enclosure url="http://example.net/b.jpg" type="image/jpeg"/>
+                    </item>
+                </channel>
+            </rss>
+XML
+            ,
+            2,
         ];
     }
 }
