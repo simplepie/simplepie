221259b58a44a977e18bcdcfb78415242515a3aa
Art4 <art4@wlabs.de>
2023-01-23 13:04:37 +0100

    Add Response tests for File class

--- tests/Fixtures/FileMock.php
+++ tests/Fixtures/FileMock.php
@@ -19,6 +19,9 @@ class FileMock extends File
         $this->headers = [
             'content-type' => 'application/atom+xml'
         ];
+        $this->parsed_headers = [
+            'content-type' => ['application/atom+xml']
+        ];
         $this->method = SimplePie::FILE_SOURCE_REMOTE;
         $this->body = '<?xml version="1.0" encoding="utf-8"?><feed xmlns="http://www.w3.org/2005/Atom" />';
         $this->status_code = 200;
