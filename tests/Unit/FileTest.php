7455f9da0e5af81f80dbfab20718d9911e8f97f8
Art4 <art4@wlabs.de>
2023-01-23 12:44:27 +0100

    File: Implement `Response` interface
    Implementing the PSR-7 inspired `Response` interface gets us one step
    closer to pluggable HTTP implementation.
    
    Most of the complexity comes from the fact that PSR-7 `MessageInterface`
    expects values of each header to always be a list, while `File` stored
    each as a single strings. But we cannot change the type of the `$headers`
    property without breaking backwards compatibility (especially for `File`
    subclasses that modify the headers), and we cannot just confidently split
    the header lines in the methods, since the concatenating multiple instances
    of the same header like `HTTP\Parser` currently does is a lossy operation.
    
    As a result, we have to check if `$headers` property changed every time
    we access the internal header state.
    
    Co-authored-by: Jan Tojnar <jtojnar@gmail.com>

--- tests/Unit/FileTest.php
+++ tests/Unit/FileTest.php
@@ -8,6 +8,9 @@ declare(strict_types=1);
 namespace SimplePie\Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
+use SimplePie\File;
+use SimplePie\HTTP\Response;
+use SimplePie\Tests\Fixtures\FileMock;
 
 class FileTest extends TestCase
 {
@@ -19,5 +22,119 @@ class FileTest extends TestCase
     public function testClassExists()
     {
         $this->assertTrue(class_exists('SimplePie_File'));
+    }
+
+    public function testFileExtendsResponse(): void
+    {
+        $this->assertInstanceOf(Response::class, new FileMock(''));
+    }
+
+    public function getResponseData(): iterable
+    {
+        yield [new FileMock('http://example.com/feed')];
+    }
+
+    /**
+     * @dataProvider getResponseData
+     */
+    public function testGetRequestedUriReturnsString(File $response): void
+    {
+        $this->assertSame(
+            'http://example.com/feed',
+            $response->get_final_requested_uri()
+        );
+    }
+
+    /**
+     * @dataProvider getResponseData
+     */
+    public function testGetStatusCodeReturnsInt(File $response): void
+    {
+        $this->assertSame(
+            200,
+            $response->get_status_code()
+        );
+    }
+
+    /**
+     * @dataProvider getResponseData
+     */
+    public function testGetHeadersReturnsArray(File $response): void
+    {
+        $this->assertSame(
+            ['content-type' => ['application/atom+xml']],
+            $response->get_headers()
+        );
+    }
+
+    /**
+     * @dataProvider getResponseData
+     */
+    public function testHasHeadersReturnsTrue(File $response): void
+    {
+        $this->assertTrue($response->has_header('Content-Type'));
+    }
+
+    /**
+     * @dataProvider getResponseData
+     */
+    public function testHasHeadersReturnsFalse(File $response): void
+    {
+        $this->assertFalse($response->has_header('X-Custom-Header'));
+    }
+
+    /**
+     * @dataProvider getResponseData
+     */
+    public function testGetHeaderReturnsArray(File $response): void
+    {
+        $this->assertSame(
+            ['application/atom+xml'],
+            $response->get_header('CONTENT-TYPE')
+        );
+    }
+
+    /**
+     * @dataProvider getResponseData
+     */
+    public function testGetHeaderReturnsEmptyArray(File $response): void
+    {
+        $this->assertSame(
+            [],
+            $response->get_header('X-Custom-Header')
+        );
+    }
+
+    /**
+     * @dataProvider getResponseData
+     */
+    public function testGetHeaderLineReturnsString(File $response): void
+    {
+        $this->assertSame(
+            'application/atom+xml',
+            $response->get_header_line('content-Type')
+        );
+    }
+
+    /**
+     * @dataProvider getResponseData
+     */
+    public function testGetHeaderLineReturnsEmptyString(File $response): void
+    {
+        $this->assertSame(
+            '',
+            $response->get_header_line('X-Custom-Header')
+        );
+    }
+
+    /**
+     * @dataProvider getResponseData
+     */
+    public function testGetBodyContentReturnsString(File $response): void
+    {
+        $this->assertSame(
+            '<?xml version="1.0" encoding="utf-8"?><feed xmlns="http://www.w3.org/2005/Atom" />',
+            $response->get_body_content()
+        );
     }
 }
