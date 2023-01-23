24a8c572b46e93076caf56d8d44b001f51e89ae2
Art4 <art4@wlabs.de>
2023-01-23 11:19:20 +0100

    Create interfaces for HTTP client and response

--- src/HTTP/Client.php
+++ src/HTTP/Client.php
@@ -43,49 +43,30 @@ declare(strict_types=1);
  * @license http://www.opensource.org/licenses/bsd-license.php BSD License
  */
 
-namespace SimplePie\Cache;
+namespace SimplePie\HTTP;
 
 /**
- * Creating a cache filename with callables
+ * HTTP Client interface
  *
  * @package SimplePie
- * @subpackage Caching
+ * @subpackage HTTP
+ * @internal
  */
-final class CallableNameFilter implements NameFilter
+interface Client
 {
-    /**
-     * @var callable
-     */
-    private $callable;
-
-    public function __construct(callable $callable)
-    {
-        $this->callable = $callable;
-    }
+    public const METHOD_GET = 'GET';
 
     /**
-     * Method to create cache filename with.
+     * send a request and return the response
      *
-     * The returning name MUST follow the rules for keys in PSR-16.
+     * @param string $method
+     * @param string $url
+     * @param array $headers in the form string[]
+     * @param array $options
      *
-     * @link https://www.php-fig.org/psr/psr-16/
+     * @return Response
      *
-     * The returning name MUST be a string of at least one character
-     * that uniquely identifies a cached item, MUST only contain the
-     * characters A-Z, a-z, 0-9, _, and . in any order in UTF-8 encoding
-     * and MUST not longer then 64 characters. The following characters
-     * are reserved for future extensions and MUST NOT be used: {}()/\@:
-     *
-     * A provided implementing library MAY support additional characters
-     * and encodings or longer lengths, but MUST support at least that
-     * minimum.
-     *
-     * @param string $name The name for the cache will be most likly an url with query string
-     *
-     * @return string the new cache name
+     * @throws HttpException if anything goes wrong requesting the data
      */
-    public function filter(string $name): string
-    {
-        return call_user_func($this->callable, $name);
-    }
+    public function request(string $method, string $url, array $headers = []): Response;
 }
