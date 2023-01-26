24d180e9ff32099a853da61e72168b904f754d1d
Art4 <art4@wlabs.de>
2023-01-26 15:24:01 +0100

    Add integration tests for basic functions

---
+++ tests/Integration/SimplePieTest.php
@@ -0,0 +1,48 @@
+<?php
+
+// SPDX-FileCopyrightText: 2004-2023 Ryan Parman, Sam Sneddon, Ryan McCue
+// SPDX-FileCopyrightText: 2023 Art4 <art4@wlabs.de>
+// SPDX-License-Identifier: BSD-3-Clause
+
+declare(strict_types=1);
+
+namespace SimplePie\Tests\Integration;
+
+use PHPUnit\Framework\TestCase;
+use SimplePie\File;
+use SimplePie\SimplePie;
+
+class SimplePieTest extends TestCase
+{
+    /**
+     * @return iterable<array{SimplePie}>
+     */
+    public function provideSimplePieInstances(): iterable
+    {
+        $filepath = dirname(__FILE__, 2) . '/data/feed_rss-2.0_for_file_mock.xml';
+
+        // Test, that requesting a local file via SimplePie->set_feed_url() works
+        $simplepie = new SimplePie();
+        $simplepie->enable_cache(false);
+        $simplepie->set_feed_url($filepath);
+
+        yield [$simplepie];
+
+        // Test, that requesting a local file via SimplePie->set_file() works
+        $simplepie = new SimplePie();
+        $simplepie->enable_cache(false);
+        $file = new File($filepath);
+        $simplepie->set_file($file);
+
+        yield [$simplepie];
+    }
+
+    /**
+     * @dataProvider provideSimplePieInstances
+     */
+    public function testSimplePieInit(SimplePie $simplepie)
+    {
+        $this->assertTrue($simplepie->init());
+        $this->assertSame(100, $simplepie->get_item_quantity());
+    }
+}
