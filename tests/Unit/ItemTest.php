simplepie/simplepie

commit 162a3d3878b755c93820ea672d83b3f6ac866f8f

Improved microformats support, expose whether the client did any last modified checks, and added support for html entities in xml.

library/SimplePie/Parser.php
@@ -128,7 +128,7 @@ class SimplePie_Parser
 			if ($declaration->parse())
 			{
 				$data = substr($data, $pos + 2);
-				$data = '<?xml version="' . $declaration->version . '" encoding="' . $encoding . '" standalone="' . (($declaration->standalone) ? 'yes' : 'no') . '"?>' . $data;
+				$data = '<?xml version="' . $declaration->version . '" encoding="' . $encoding . '" standalone="' . (($declaration->standalone) ? 'yes' : 'no') . '"?>' ."\n". $this->declare_html_entities() . $data;
 			}
 			else
 			{
@@ -413,62 +413,136 @@ class SimplePie_Parser
 		return $cache[$string];
 	}
 
+	private function parse_hcard($data) {
+		$name = '';
+		$link = '';
+		// Check if h-card is set and pass that information on in the link.
+		if (isset($data['type']) && in_array('h-card', $data['type'])) {
+			if (isset($data['properties']['name'][0])) {
+				$name = $data['properties']['name'][0];
+			}
+			if (isset($data['properties']['url'][0])) {
+				$link = $data['properties']['url'][0];
+				if ($name === '') {
+					$name = $link;
+				}
+				else {
+					// can't have commas in categories.
+					$name = str_replace(',', '', $name);
+				}
+				return '<a class="h-card" href="'.$link.'">'.$name.'</a>';
+			}
+		}
+		return isset($data['value']) ? $data['value'] : '';
+	}
+
 	private function parse_microformats(&$data, $url) {
 		if (!function_exists('Mf2\parse')) return false;
 
+		$feed_title = '';
+		$icon = '';
+		$items = array();
+		$entries = array();
 		$mf = Mf2\parse($data, $url);
-		foreach ($mf['items'] as $microformat) {
-			if (in_array('h-entry', $microformat['type'])) {
-				$entry = array();
-				if (isset($microformat['properties']['url'][0])) {
-					$link = $microformat['properties']['url'][0];
-					$entry['link'] = array(array('data' => $link));
+		if (isset($mf['items'][0]['type'])) {
+			// If this item is an h-feed, look for it's h-entry children.
+			if (in_array('h-feed', $mf['items'][0]['type'])) {
+				$entries = $mf['items'][0]['children'];
+				// Also set the feed title and icon from the h-feed if available.
+				if (isset($mf['items'][0]['properties']['name'][0])) {
+					$feed_title = $mf['items'][0]['properties']['name'][0];
 				}
-				if (isset($microformat['properties']['name'][0])) {
-					$title = htmlspecialchars($microformat['properties']['name'][0]);
-					$entry['title'] = array(array('data' => $title));
-				}
-				if (isset($microformat['properties']['author'][0])) {
-					$author = $microformat['properties']['author'][0];
-					// author is a special case, it can be plain text or an h-card array.
-					if (is_array($author)) {
-						$author = isset($author['value']) ? $author['value'] : '';
+				if (isset($mf['items'][0]['properties']['author'][0])) {
+					$author = $mf['items'][0]['properties']['author'][0];
+					if (is_array($author) &&
+							isset($author['type']) && in_array('h-card', $author['type'])) {
+						if (isset($author['properties']['photo'][0])) {
+							$icon = $author['properties']['photo'][0];
+						}
 					}
-					$entry['author'] = array(array('data' => htmlspecialchars($author)));
 				}
-				if (isset($microformat['properties']['content'][0]['html'])) {
-					$description = $microformat['properties']['content'][0]['html'];
-					$entry['description'] = array(array('data' => $description));
+			}
+			else {
+				$entries = $mf['items'];
+			}
+		}
+		foreach ($entries as $entry) {
+			if (in_array('h-entry', $entry['type'])) {
+				$item = array();
+				if (isset($entry['properties']['url'][0])) {
+					$link = $entry['properties']['url'][0];
+					$item['link'] = array(array('data' => $link));
 				}
-				if (isset($microformat['properties']['category'])) {
-					$category = implode(',', $microformat['properties']['category']);
-					$category = htmlspecialchars($category);
-					$entry['category'] = array(array('data' => $category));
+				if (isset($entry['properties']['name'][0])) {
+					$title = $entry['properties']['name'][0];
+					$item['title'] = array(array('data' => $title));
 				}
-				if (isset($microformat['properties']['published'][0])) {
-					$timestamp = strtotime($microformat['properties']['published'][0]);
+				if (isset($entry['properties']['author'][0])) {
+					// author is a special case, it can be plain text or an h-card array.
+					$author = $entry['properties']['author'][0];
+					if (is_array($author)) {
+						$author = $this->parse_hcard($author);
+					}
+					$item['author'] = array(array('data' => $author));
+				}
+				if (isset($entry['properties']['content'][0]['html'])) {
+					$description = $entry['properties']['content'][0]['html'];
+					$item['description'] = array(array('data' => $description));
+				}
+				if (isset($entry['properties']['category'])) {
+					$category_csv = '';
+					// categories can also contain h-cards.
+					foreach ($entry['properties']['category'] as $category) {
+						if ($category_csv !== '') $category_csv .= ', ';
+						if (is_array($category)) {
+							$category_csv .= $this->parse_hcard($category);
+						}
+						else {
+							// can't have commas in categories.
+							$category_csv .= str_replace(',', '', $category);
+						}
+					}
+					$item['category'] = array(array('data' => $category_csv));
+				}
+				if (isset($entry['properties']['published'][0])) {
+					$timestamp = strtotime($entry['properties']['published'][0]);
 					$pub_date = date('F j Y g:ia', $timestamp).' GMT';
-					$entry['pubDate'] = array(array('data' => $pub_date));
+					$item['pubDate'] = array(array('data' => $pub_date));
 				}
-				$items[] = array('child' => array('' => $entry));
+				$items[] = array('child' => array('' => $item));
 			}
 		}
 		// Mimic RSS data format when storing microformats.
 		$link = array(array('data' => $url));
-		// Get the title from the html, or use the url again if not found.
-		if ($position = strpos($data, '<title>')) {
+		$image = '';
+		if ($icon !== '') {
+			array(array('child' => array('' =>
+			      array('url' => array(array('data' => $icon))))));
+		}
+		// Use the a name given for the h-feed, or get the title from the html.
+		if ($feed_title !== '') {
+			$feed_title = array(array('data' => htmlspecialchars($feed_title)));
+		}
+		else if ($position = strpos($data, '<title>')) {
 			$start = $position < 200 ? 0 : $position - 200;
 			$check = substr($data, $start, 400);
 			$matches = array();
 			if (preg_match('/<title>(.+)<\/title>/', $check, $matches)) {
-				$title = array(array('data' => htmlspecialchars($matches[1])));
+				$feed_title = array(array('data' => htmlspecialchars($matches[1])));
 			}
 		}
 		$channel = array('channel' => array(array('child' => array('' =>
-			array('link' => $link, 'title' => $title, 'item' => $items)))));
+			array('link' => $link, 'image' => $image, 'title' => $feed_title,
+			      'item' => $items)))));
 		$rss = array(array('attribs' => array('' => array('version' => '2.0')),
 		                   'child' => array('' => $channel)));
 		$this->data = array('child' => array('' => array('rss' => $rss)));
 		return true;
+	}
+
+	private function declare_html_entities() {
+		// This is required because the RSS specification says that entity-encoded
+		// html is allowed, but the xml specification says they must be declared.
+		return '<!DOCTYPE html [ <!ENTITY nbsp "&#x00A0;"> <!ENTITY iexcl "&#x00A1;"> <!ENTITY cent "&#x00A2;"> <!ENTITY pound "&#x00A3;"> <!ENTITY curren "&#x00A4;"> <!ENTITY yen "&#x00A5;"> <!ENTITY brvbar "&#x00A6;"> <!ENTITY sect "&#x00A7;"> <!ENTITY uml "&#x00A8;"> <!ENTITY copy "&#x00A9;"> <!ENTITY ordf "&#x00AA;"> <!ENTITY laquo "&#x00AB;"> <!ENTITY not "&#x00AC;"> <!ENTITY shy "&#x00AD;"> <!ENTITY reg "&#x00AE;"> <!ENTITY macr "&#x00AF;"> <!ENTITY deg "&#x00B0;"> <!ENTITY plusmn "&#x00B1;"> <!ENTITY sup2 "&#x00B2;"> <!ENTITY sup3 "&#x00B3;"> <!ENTITY acute "&#x00B4;"> <!ENTITY micro "&#x00B5;"> <!ENTITY para "&#x00B6;"> <!ENTITY middot "&#x00B7;"> <!ENTITY cedil "&#x00B8;"> <!ENTITY sup1 "&#x00B9;"> <!ENTITY ordm "&#x00BA;"> <!ENTITY raquo "&#x00BB;"> <!ENTITY frac14 "&#x00BC;"> <!ENTITY frac12 "&#x00BD;"> <!ENTITY frac34 "&#x00BE;"> <!ENTITY iquest "&#x00BF;"> <!ENTITY Agrave "&#x00C0;"> <!ENTITY Aacute "&#x00C1;"> <!ENTITY Acirc "&#x00C2;"> <!ENTITY Atilde "&#x00C3;"> <!ENTITY Auml "&#x00C4;"> <!ENTITY Aring "&#x00C5;"> <!ENTITY AElig "&#x00C6;"> <!ENTITY Ccedil "&#x00C7;"> <!ENTITY Egrave "&#x00C8;"> <!ENTITY Eacute "&#x00C9;"> <!ENTITY Ecirc "&#x00CA;"> <!ENTITY Euml "&#x00CB;"> <!ENTITY Igrave "&#x00CC;"> <!ENTITY Iacute "&#x00CD;"> <!ENTITY Icirc "&#x00CE;"> <!ENTITY Iuml "&#x00CF;"> <!ENTITY ETH "&#x00D0;"> <!ENTITY Ntilde "&#x00D1;"> <!ENTITY Ograve "&#x00D2;"> <!ENTITY Oacute "&#x00D3;"> <!ENTITY Ocirc "&#x00D4;"> <!ENTITY Otilde "&#x00D5;"> <!ENTITY Ouml "&#x00D6;"> <!ENTITY times "&#x00D7;"> <!ENTITY Oslash "&#x00D8;"> <!ENTITY Ugrave "&#x00D9;"> <!ENTITY Uacute "&#x00DA;"> <!ENTITY Ucirc "&#x00DB;"> <!ENTITY Uuml "&#x00DC;"> <!ENTITY Yacute "&#x00DD;"> <!ENTITY THORN "&#x00DE;"> <!ENTITY szlig "&#x00DF;"> <!ENTITY agrave "&#x00E0;"> <!ENTITY aacute "&#x00E1;"> <!ENTITY acirc "&#x00E2;"> <!ENTITY atilde "&#x00E3;"> <!ENTITY auml "&#x00E4;"> <!ENTITY aring "&#x00E5;"> <!ENTITY aelig "&#x00E6;"> <!ENTITY ccedil "&#x00E7;"> <!ENTITY egrave "&#x00E8;"> <!ENTITY eacute "&#x00E9;"> <!ENTITY ecirc "&#x00EA;"> <!ENTITY euml "&#x00EB;"> <!ENTITY igrave "&#x00EC;"> <!ENTITY iacute "&#x00ED;"> <!ENTITY icirc "&#x00EE;"> <!ENTITY iuml "&#x00EF;"> <!ENTITY eth "&#x00F0;"> <!ENTITY ntilde "&#x00F1;"> <!ENTITY ograve "&#x00F2;"> <!ENTITY oacute "&#x00F3;"> <!ENTITY ocirc "&#x00F4;"> <!ENTITY otilde "&#x00F5;"> <!ENTITY ouml "&#x00F6;"> <!ENTITY divide "&#x00F7;"> <!ENTITY oslash "&#x00F8;"> <!ENTITY ugrave "&#x00F9;"> <!ENTITY uacute "&#x00FA;"> <!ENTITY ucirc "&#x00FB;"> <!ENTITY uuml "&#x00FC;"> <!ENTITY yacute "&#x00FD;"> <!ENTITY thorn "&#x00FE;"> <!ENTITY yuml "&#x00FF;"> <!ENTITY OElig "&#x0152;"> <!ENTITY oelig "&#x0153;"> <!ENTITY Scaron "&#x0160;"> <!ENTITY scaron "&#x0161;"> <!ENTITY Yuml "&#x0178;"> <!ENTITY fnof "&#x0192;"> <!ENTITY circ "&#x02C6;"> <!ENTITY tilde "&#x02DC;"> <!ENTITY Alpha "&#x0391;"> <!ENTITY Beta "&#x0392;"> <!ENTITY Gamma "&#x0393;"> <!ENTITY Epsilon "&#x0395;"> <!ENTITY Zeta "&#x0396;"> <!ENTITY Eta "&#x0397;"> <!ENTITY Theta "&#x0398;"> <!ENTITY Iota "&#x0399;"> <!ENTITY Kappa "&#x039A;"> <!ENTITY Lambda "&#x039B;"> <!ENTITY Mu "&#x039C;"> <!ENTITY Nu "&#x039D;"> <!ENTITY Xi "&#x039E;"> <!ENTITY Omicron "&#x039F;"> <!ENTITY Pi "&#x03A0;"> <!ENTITY Rho "&#x03A1;"> <!ENTITY Sigma "&#x03A3;"> <!ENTITY Tau "&#x03A4;"> <!ENTITY Upsilon "&#x03A5;"> <!ENTITY Phi "&#x03A6;"> <!ENTITY Chi "&#x03A7;"> <!ENTITY Psi "&#x03A8;"> <!ENTITY Omega "&#x03A9;"> <!ENTITY alpha "&#x03B1;"> <!ENTITY beta "&#x03B2;"> <!ENTITY gamma "&#x03B3;"> <!ENTITY delta "&#x03B4;"> <!ENTITY epsilon "&#x03B5;"> <!ENTITY zeta "&#x03B6;"> <!ENTITY eta "&#x03B7;"> <!ENTITY theta "&#x03B8;"> <!ENTITY iota "&#x03B9;"> <!ENTITY kappa "&#x03BA;"> <!ENTITY lambda "&#x03BB;"> <!ENTITY mu "&#x03BC;"> <!ENTITY nu "&#x03BD;"> <!ENTITY xi "&#x03BE;"> <!ENTITY omicron "&#x03BF;"> <!ENTITY pi "&#x03C0;"> <!ENTITY rho "&#x03C1;"> <!ENTITY sigmaf "&#x03C2;"> <!ENTITY sigma "&#x03C3;"> <!ENTITY tau "&#x03C4;"> <!ENTITY upsilon "&#x03C5;"> <!ENTITY phi "&#x03C6;"> <!ENTITY chi "&#x03C7;"> <!ENTITY psi "&#x03C8;"> <!ENTITY omega "&#x03C9;"> <!ENTITY thetasym "&#x03D1;"> <!ENTITY upsih "&#x03D2;"> <!ENTITY piv "&#x03D6;"> <!ENTITY ensp "&#x2002;"> <!ENTITY emsp "&#x2003;"> <!ENTITY thinsp "&#x2009;"> <!ENTITY zwnj "&#x200C;"> <!ENTITY zwj "&#x200D;"> <!ENTITY lrm "&#x200E;"> <!ENTITY rlm "&#x200F;"> <!ENTITY ndash "&#x2013;"> <!ENTITY mdash "&#x2014;"> <!ENTITY lsquo "&#x2018;"> <!ENTITY rsquo "&#x2019;"> <!ENTITY sbquo "&#x201A;"> <!ENTITY ldquo "&#x201C;"> <!ENTITY rdquo "&#x201D;"> <!ENTITY bdquo "&#x201E;"> <!ENTITY dagger "&#x2020;"> <!ENTITY Dagger "&#x2021;"> <!ENTITY bull "&#x2022;"> <!ENTITY hellip "&#x2026;"> <!ENTITY permil "&#x2030;"> <!ENTITY prime "&#x2032;"> <!ENTITY Prime "&#x2033;"> <!ENTITY lsaquo "&#x2039;"> <!ENTITY rsaquo "&#x203A;"> <!ENTITY oline "&#x203E;"> <!ENTITY frasl "&#x2044;"> <!ENTITY euro "&#x20AC;"> <!ENTITY image "&#x2111;"> <!ENTITY weierp "&#x2118;"> <!ENTITY real "&#x211C;"> <!ENTITY trade "&#x2122;"> <!ENTITY alefsym "&#x2135;"> <!ENTITY larr "&#x2190;"> <!ENTITY uarr "&#x2191;"> <!ENTITY rarr "&#x2192;"> <!ENTITY darr "&#x2193;"> <!ENTITY harr "&#x2194;"> <!ENTITY crarr "&#x21B5;"> <!ENTITY lArr "&#x21D0;"> <!ENTITY uArr "&#x21D1;"> <!ENTITY rArr "&#x21D2;"> <!ENTITY dArr "&#x21D3;"> <!ENTITY hArr "&#x21D4;"> <!ENTITY forall "&#x2200;"> <!ENTITY part "&#x2202;"> <!ENTITY exist "&#x2203;"> <!ENTITY empty "&#x2205;"> <!ENTITY nabla "&#x2207;"> <!ENTITY isin "&#x2208;"> <!ENTITY notin "&#x2209;"> <!ENTITY ni "&#x220B;"> <!ENTITY prod "&#x220F;"> <!ENTITY sum "&#x2211;"> <!ENTITY minus "&#x2212;"> <!ENTITY lowast "&#x2217;"> <!ENTITY radic "&#x221A;"> <!ENTITY prop "&#x221D;"> <!ENTITY infin "&#x221E;"> <!ENTITY ang "&#x2220;"> <!ENTITY and "&#x2227;"> <!ENTITY or "&#x2228;"> <!ENTITY cap "&#x2229;"> <!ENTITY cup "&#x222A;"> <!ENTITY int "&#x222B;"> <!ENTITY there4 "&#x2234;"> <!ENTITY sim "&#x223C;"> <!ENTITY cong "&#x2245;"> <!ENTITY asymp "&#x2248;"> <!ENTITY ne "&#x2260;"> <!ENTITY equiv "&#x2261;"> <!ENTITY le "&#x2264;"> <!ENTITY ge "&#x2265;"> <!ENTITY sub "&#x2282;"> <!ENTITY sup "&#x2283;"> <!ENTITY nsub "&#x2284;"> <!ENTITY sube "&#x2286;"> <!ENTITY supe "&#x2287;"> <!ENTITY oplus "&#x2295;"> <!ENTITY otimes "&#x2297;"> <!ENTITY perp "&#x22A5;"> <!ENTITY sdot "&#x22C5;"> <!ENTITY lceil "&#x2308;"> <!ENTITY rceil "&#x2309;"> <!ENTITY lfloor "&#x230A;"> <!ENTITY rfloor "&#x230B;"> <!ENTITY lang "&#x2329;"> <!ENTITY rang "&#x232A;"> <!ENTITY loz "&#x25CA;"> <!ENTITY spades "&#x2660;"> <!ENTITY clubs "&#x2663;"> <!ENTITY hearts "&#x2665;"> <!ENTITY diams "&#x2666;"> ]>';
 	}
 }

library/SimplePie.php
@@ -608,6 +608,8 @@ class SimplePie
 	 */
 	public $item_limit = 0;
 
+	public $check_modified = false;
+
 	/**
 	 * @var array Stores the default attributes to be stripped by strip_attributes().
 	 * @see SimplePie::strip_attributes()
@@ -1321,6 +1323,7 @@ class SimplePie
 
 		$this->error = null;
 		$this->data = array();
+		$this->check_modified = false;
 		$this->multifeed_objects = array();
 		$cache = false;
 
@@ -1381,7 +1384,7 @@ class SimplePie
 			// Text MIME-type default
 			elseif (substr($sniffed, 0, 5) === 'text/')
 			{
-				$encodings[] = 'US-ASCII';
+				$encodings[] = 'UTF-8';
 			}
 		}
 
@@ -1493,7 +1496,10 @@ class SimplePie
 				// Check if the cache has been updated
 				elseif ($cache->mtime() + $this->cache_duration < time())
 				{
-					// If we have last-modified and/or etag set
+					// Want to know if we tried to send last-modified and/or etag headers
+					// when requesting this file. (Note that it's up to the file to
+					// support this, but we don't always send the headers either.)
+					$this->check_modified = true;
 					if (isset($this->data['headers']['last-modified']) || isset($this->data['headers']['etag']))
 					{
 						$headers = array(
@@ -1514,12 +1520,16 @@ class SimplePie
 						{
 							if ($file->status_code === 304)
 							{
+								// Set raw_data to false here too, to signify that the cache
+								// is still valid.
+								$this->raw_data = false;
 								$cache->touch();
 								return true;
 							}
 						}
 						else
 						{
+							$this->check_modified = false;
 							unset($file);
 						}
 					}

library/SimplePie/Item.php
@@ -455,15 +455,15 @@ class SimplePie_Item
 			$label = null;
 			if (isset($category['attribs']['']['term']))
 			{
-				$term = $this->sanitize($category['attribs']['']['term'], SIMPLEPIE_CONSTRUCT_TEXT);
+				$term = $this->sanitize($category['attribs']['']['term'], SIMPLEPIE_CONSTRUCT_HTML);
 			}
 			if (isset($category['attribs']['']['scheme']))
 			{
-				$scheme = $this->sanitize($category['attribs']['']['scheme'], SIMPLEPIE_CONSTRUCT_TEXT);
+				$scheme = $this->sanitize($category['attribs']['']['scheme'], SIMPLEPIE_CONSTRUCT_HTML);
 			}
 			if (isset($category['attribs']['']['label']))
 			{
-				$label = $this->sanitize($category['attribs']['']['label'], SIMPLEPIE_CONSTRUCT_TEXT);
+				$label = $this->sanitize($category['attribs']['']['label'], SIMPLEPIE_CONSTRUCT_HTML);
 			}
 			$categories[] = $this->registry->create('Category', array($term, $scheme, $label));
 		}
@@ -471,10 +471,10 @@ class SimplePie_Item
 		{
 			// This is really the label, but keep this as the term also for BC.
 			// Label will also work on retrieving because that falls back to term.
-			$term = $this->sanitize($category['data'], SIMPLEPIE_CONSTRUCT_TEXT);
+			$term = $this->sanitize($category['data'], SIMPLEPIE_CONSTRUCT_HTML);
 			if (isset($category['attribs']['']['domain']))
 			{
-				$scheme = $this->sanitize($category['attribs']['']['domain'], SIMPLEPIE_CONSTRUCT_TEXT);
+				$scheme = $this->sanitize($category['attribs']['']['domain'], SIMPLEPIE_CONSTRUCT_HTML);
 			}
 			else
 			{
@@ -484,11 +484,11 @@ class SimplePie_Item
 		}
 		foreach ((array) $this->get_item_tags(SIMPLEPIE_NAMESPACE_DC_11, 'subject') as $category)
 		{
-			$categories[] = $this->registry->create('Category', array($this->sanitize($category['data'], SIMPLEPIE_CONSTRUCT_TEXT), null, null));
+			$categories[] = $this->registry->create('Category', array($this->sanitize($category['data'], SIMPLEPIE_CONSTRUCT_HTML), null, null));
 		}
 		foreach ((array) $this->get_item_tags(SIMPLEPIE_NAMESPACE_DC_10, 'subject') as $category)
 		{
-			$categories[] = $this->registry->create('Category', array($this->sanitize($category['data'], SIMPLEPIE_CONSTRUCT_TEXT), null, null));
+			$categories[] = $this->registry->create('Category', array($this->sanitize($category['data'], SIMPLEPIE_CONSTRUCT_HTML), null, null));
 		}
 
 		if (!empty($categories))
@@ -625,7 +625,7 @@ class SimplePie_Item
 			$email = null;
 			if (isset($author['child'][SIMPLEPIE_NAMESPACE_ATOM_10]['name'][0]['data']))
 			{
-				$name = $this->sanitize($author['child'][SIMPLEPIE_NAMESPACE_ATOM_10]['name'][0]['data'], SIMPLEPIE_CONSTRUCT_TEXT);
+				$name = $this->sanitize($author['child'][SIMPLEPIE_NAMESPACE_ATOM_10]['name'][0]['data'], SIMPLEPIE_CONSTRUCT_HTML);
 			}
 			if (isset($author['child'][SIMPLEPIE_NAMESPACE_ATOM_10]['uri'][0]['data']))
 			{
@@ -633,7 +633,7 @@ class SimplePie_Item
 			}
 			if (isset($author['child'][SIMPLEPIE_NAMESPACE_ATOM_10]['email'][0]['data']))
 			{
-				$email = $this->sanitize($author['child'][SIMPLEPIE_NAMESPACE_ATOM_10]['email'][0]['data'], SIMPLEPIE_CONSTRUCT_TEXT);
+				$email = $this->sanitize($author['child'][SIMPLEPIE_NAMESPACE_ATOM_10]['email'][0]['data'], SIMPLEPIE_CONSTRUCT_HTML);
 			}
 			if ($name !== null || $email !== null || $uri !== null)
 			{
@@ -647,7 +647,7 @@ class SimplePie_Item
 			$email = null;
 			if (isset($author[0]['child'][SIMPLEPIE_NAMESPACE_ATOM_03]['name'][0]['data']))
 			{
-				$name = $this->sanitize($author[0]['child'][SIMPLEPIE_NAMESPACE_ATOM_03]['name'][0]['data'], SIMPLEPIE_CONSTRUCT_TEXT);
+				$name = $this->sanitize($author[0]['child'][SIMPLEPIE_NAMESPACE_ATOM_03]['name'][0]['data'], SIMPLEPIE_CONSTRUCT_HTML);
 			}
 			if (isset($author[0]['child'][SIMPLEPIE_NAMESPACE_ATOM_03]['url'][0]['data']))
 			{
@@ -655,7 +655,7 @@ class SimplePie_Item
 			}
 			if (isset($author[0]['child'][SIMPLEPIE_NAMESPACE_ATOM_03]['email'][0]['data']))
 			{
-				$email = $this->sanitize($author[0]['child'][SIMPLEPIE_NAMESPACE_ATOM_03]['email'][0]['data'], SIMPLEPIE_CONSTRUCT_TEXT);
+				$email = $this->sanitize($author[0]['child'][SIMPLEPIE_NAMESPACE_ATOM_03]['email'][0]['data'], SIMPLEPIE_CONSTRUCT_HTML);
 			}
 			if ($name !== null || $email !== null || $url !== null)
 			{
@@ -664,19 +664,19 @@ class SimplePie_Item
 		}
 		if ($author = $this->get_item_tags(SIMPLEPIE_NAMESPACE_RSS_20, 'author'))
 		{
-			$authors[] = $this->registry->create('Author', array(null, null, $this->sanitize($author[0]['data'], SIMPLEPIE_CONSTRUCT_TEXT)));
+			$authors[] = $this->registry->create('Author', array(null, null, $this->sanitize($author[0]['data'], SIMPLEPIE_CONSTRUCT_HTML)));
 		}
 		foreach ((array) $this->get_item_tags(SIMPLEPIE_NAMESPACE_DC_11, 'creator') as $author)
 		{
-			$authors[] = $this->registry->create('Author', array($this->sanitize($author['data'], SIMPLEPIE_CONSTRUCT_TEXT), null, null));
+			$authors[] = $this->registry->create('Author', array($this->sanitize($author['data'], SIMPLEPIE_CONSTRUCT_HTML), null, null));
 		}
 		foreach ((array) $this->get_item_tags(SIMPLEPIE_NAMESPACE_DC_10, 'creator') as $author)
 		{
-			$authors[] = $this->registry->create('Author', array($this->sanitize($author['data'], SIMPLEPIE_CONSTRUCT_TEXT), null, null));
+			$authors[] = $this->registry->create('Author', array($this->sanitize($author['data'], SIMPLEPIE_CONSTRUCT_HTML), null, null));
 		}
 		foreach ((array) $this->get_item_tags(SIMPLEPIE_NAMESPACE_ITUNES, 'author') as $author)
 		{
-			$authors[] = $this->registry->create('Author', array($this->sanitize($author['data'], SIMPLEPIE_CONSTRUCT_TEXT), null, null));
+			$authors[] = $this->registry->create('Author', array($this->sanitize($author['data'], SIMPLEPIE_CONSTRUCT_HTML), null, null));
 		}
 
 		if (!empty($authors))

library/SimplePie/Misc.php
@@ -124,7 +124,7 @@ class SimplePie_Misc
 						{
 							$attribs[$j][2] = $attribs[$j][1];
 						}
-						$return[$i]['attribs'][strtolower($attribs[$j][1])]['data'] = SimplePie_Misc::entities_decode(end($attribs[$j]), 'UTF-8');
+						$return[$i]['attribs'][strtolower($attribs[$j][1])]['data'] = SimplePie_Misc::entities_decode(end($attribs[$j]));
 					}
 				}
 			}

commit 034030924c249f4cb5b994158a70d17ebc74078a

Fix support for feeds with XML preample + DTD, entities (#915)
Alternative of https://github.com/simplepie/simplepie/pull/914 with support for XML feeds using HTML entities

Simple feeds with XML preamble + DOCTYPE are crashing (see example in test) due to regression from https://github.com/simplepie/simplepie/commit/162a3d3878b755c93820ea672d83b3f6ac866f8f
Its implementation is buggy, as it creates a second DOCTYPE declaration even when there is an existing one, which is completely invalid.

Example of feed: https://blog.plover.com/index.rss
Downstream Issue: https://github.com/FreshRSS/FreshRSS/issues/7514
Downstream PR: https://github.com/FreshRSS/simplepie/pull/35

Note that the culprit feature *added support for html entities in xml* seems to come from a misunderstanding of
https://www.rssboard.org/rss-encoding-examples
HTML entities are only allowed in XML when there is a DTD declaring them. SimplePie is even allowing Atom documents with undeclared HTML entities - which I am not sure is on purpose.

Downstream issue: https://github.com/FreshRSS/FreshRSS/issues/7687

src/Parser.php
@@ -111,11 +111,14 @@ class Parser implements RegistryAware
             $declaration = $this->registry->create(DeclarationParser::class, [substr($data, 5, $pos - 5)]);
             if ($declaration->parse()) {
                 $data = substr($data, $pos + 2);
-                $data = '<?xml version="' . $declaration->version . '" encoding="' . $encoding . '" standalone="' . (($declaration->standalone) ? 'yes' : 'no') . '"?>' ."\n". $this->declare_html_entities() . $data;
+                $data = '<?xml version="' . $declaration->version . '" encoding="' . $encoding . '" standalone="' . (($declaration->standalone) ? 'yes' : 'no') . '"?>' . "\n" .
+                    $this->set_doctype($data);
             } else {
                 $this->error_string = 'SimplePie bug! Please report this!';
                 return false;
             }
+        } else {
+            $data = $this->set_doctype($data);
         }
 
         $return = true;
@@ -642,6 +645,15 @@ class Parser implements RegistryAware
                            'child' => ['' => $channel]]];
         $this->data = ['child' => ['' => ['rss' => $rss]]];
         return true;
+    }
+
+    private function set_doctype(string $data): string
+    {
+        // Strip DOCTYPE except if containing an [internal subset]
+        $data = preg_replace('/^\\s*<!DOCTYPE\\s[^>\\[\\]]*>\s*/', '', $data);
+        // Declare HTML entities only if no remaining DOCTYPE
+        $doctype = preg_match('/^\\s*<!DOCTYPE\\s/', $data) ? '' : $this->declare_html_entities();
+        return $doctype . $data;
     }
 
     private function declare_html_entities(): string

tests/Unit/ItemTest.php
@@ -4312,6 +4312,93 @@ EOT
                 ,
                 'Item Title',
             ],
+            'Test RSS 0.91 XML and DTD' => [
+<<<XML
+<?xml version="1.0"?>
+<!DOCTYPE rss PUBLIC "-//Netscape Communications//DTD RSS 0.91//EN" "http://www.rssboard.org/rss-0.91.dtd">
+<rss version="0.91">
+	<channel>
+		<item>
+			<title>Item Title</title>
+		</item>
+	</channel>
+</rss>
+XML
+                ,
+                'Item Title',
+            ],
+            'Test RSS 0.91 XML and DTD and external HTML entity' => [
+<<<XML
+<?xml version="1.0"?>
+<!DOCTYPE rss PUBLIC "-//Netscape Communications//DTD RSS 0.91//EN" "http://www.rssboard.org/rss-0.91.dtd">
+<rss version="0.91">
+	<channel>
+		<item>
+			<title>caf&eacute;</title>
+		</item>
+	</channel>
+</rss>
+XML
+                ,
+                'café',
+            ],
+            'Test RSS 0.91 DTD and external HTML entity' => [
+<<<XML
+<!DOCTYPE rss PUBLIC "-//Netscape Communications//DTD RSS 0.91//EN" "http://www.rssboard.org/rss-0.91.dtd">
+<rss version="0.91">
+	<channel>
+		<item>
+			<title>caf&eacute;</title>
+		</item>
+	</channel>
+</rss>
+XML
+                ,
+                'café',
+            ],
+            'Test RSS 0.91 internal DTD with internal entity' => [
+<<<XML
+<!DOCTYPE rss [
+	<!ENTITY helloworld "Hello World!">
+]>
+<rss version="0.91">
+	<channel>
+		<item>
+			<title>&helloworld;</title>
+		</item>
+	</channel>
+</rss>
+XML
+                ,
+                'Hello World!',
+            ],
+            'Test RSS 0.91 XML and undeclared HTML entity (invalid)' => [
+<<<XML
+<?xml version="1.0"?>
+<rss version="0.91">
+	<channel>
+		<item>
+			<title>caf&eacute;</title>
+		</item>
+	</channel>
+</rss>
+XML
+                ,
+                'café',
+            ],
+            'Test RSS 0.91 with undeclared HTML entity (invalid)' => [
+<<<XML
+<rss version="0.91">
+	<channel>
+		<item>
+			<title>caf&eacute;</title>
+		</item>
+	</channel>
+</rss>
+XML
+                ,
+                'café',
+            ],
             'Test RSS 0.91-Userland Atom 0.3 Title' => [
 <<<EOT
 <rss version="0.91" xmlns:a="http://purl.org/atom/ns#">